c82a7899852450b18f8c5424f7f4316f270393c0
Robin Harms Oredsson <robin@betahaus.net>
2011-05-03 09:42:20 +0200

    initial import

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'repoze.zodbconn',
+    'repoze.tm2>=1.0b1', # default_commit_veto
+    'repoze.retry',
+    'ZODB3',
+    'WebError',
+    'repoze.folder',
+    'deform',
+    'repoze.whoplugins.zodb',
+    'Babel',
+    ]
+
+setup(name='voteit.core',
+      version='0.0',
+      description='Core VoteIT package',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = requires,
+      tests_require= requires,
+      test_suite="voteit.core",
+      entry_points = """\
+      [paste.app_factory]
+      main = voteit.core:main
+      """,
+      paster_plugins=['pyramid'],
+      message_extractors = { '.': [
+              ('**.py',   'chameleon_python', None ),
+              ('**.pt',   'chameleon_xml', None ),
+              ]},
+      )
