284a8cf5f3689f6a5493d630c7dfcc1225b1b879
Robin Harms Oredsson <robin@betahaus.net>
2011-05-06 18:59:31 +0200

    Fixed typo in bootstrap

---
+++ voteit/core/bootstrap.py
@@ -0,0 +1,34 @@
+
+from voteit.core.models.site import SiteRoot
+from voteit.core.models.users import Users
+from voteit.core.models.user import User
+from voteit.core import VoteITMF as _
+from voteit.core.security import ROLE_ADMIN
+
+
+def bootstrap():
+    """ Bootstrap site root.
+        Will add:
+        - Site root
+        - Users folder
+        - An administrative user with login: admin and pass: admin
+    """
+    print "Bootstrapping site - creating 'admin' user with password 'admin'"
+    root = SiteRoot()
+    root.title = _(u"VoteIT")
+
+    #Add users folder
+    root['users'] = Users()
+    users = root.users
+    users.title = _(u"Registered users")
+    
+    #Add user admin
+    admin = User()
+    admin.set_password('admin')
+    admin.set_field_value('first_name', 'Administrator')
+    users['admin'] = admin
+    
+    #Add admin to group managers
+    root.add_groups('admin', [ROLE_ADMIN])
+    
+    return root
