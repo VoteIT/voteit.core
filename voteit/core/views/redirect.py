5c200cb765f9097eb14a402c573dfdde58a8d18d
Robin Harms Oredsson <robin@betahaus.net>
2011-05-25 14:47:10 +0200

    Redirect to agenda item from proposals

---
+++ voteit/core/views/redirect.py
@@ -0,0 +1,18 @@
+from pyramid.traversal import find_interface
+from pyramid.view import view_config
+from pyramid.url import resource_url
+from webob.exc import HTTPFound
+from pyramid.exceptions import NotFound
+
+from voteit.core.models.interfaces import IAgendaItem
+from voteit.core.models.interfaces import IProposal
+
+
+@view_config(context=IProposal)
+def redirect_to_agenda_item(context, request):
+    ai = find_interface(context, IAgendaItem)
+    if ai:
+        url = resource_url(ai, request)
+        return HTTPFound(location=url)
+    raise NotFound("Couldn't locate Agenda Item from this context.")
+
