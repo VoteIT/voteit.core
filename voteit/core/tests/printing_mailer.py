f7bd7673df4ee86e2e54e1625d259f7e50a79b19
Robin Harms Oredsson <robin@betahaus.net>
2011-05-31 18:32:46 +0200

    printing mailer to make visual testing easier

---
+++ voteit/core/tests/printing_mailer.py
@@ -0,0 +1,26 @@
+from pyramid_mailer.interfaces import IMailer
+
+
+class PrintingMailer(object):
+    """
+    Dummy mailing instance. Simply prints all messages directly instead of handling them.
+    Good for avoiding mailing users when you want to test things locally.
+    """
+
+    def send(self, message):    
+        """
+        Send, but really print content of message
+        """
+        
+        print "Subject: %s" % message.subject
+        print "To: %s" % ", ".join(message.recipients)
+        print "======================================"
+        print message.body
+
+    send_to_queue = send_immediately = send
+
+
+def includeme(config):
+    print "\nWARNING! Using printing mailer - no mail will be sent!\n"
+    mailer = PrintingMailer()
+    config.registry.registerUtility(mailer, IMailer)
