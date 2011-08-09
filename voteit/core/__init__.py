d54f83348921f382060f19bb69c2c6465d557289
Fredrik Nilson <fredrik@betahaus.net>
2011-08-09 10:14:08 +0200

    making the widget work better

--- voteit/core/__init__.py
+++ voteit/core/__init__.py
@@ -46,7 +46,7 @@ def main(global_config, **settings):
     config.add_static_view('static', '%s:static' % PROJECTNAME)
     config.add_static_view('deform', 'deform:static')
 
-    #Set which mailer to use        
+    #Set which mailer to use
     config.include(settings['mailer'])
 
     #config.add_translation_dirs('%s:locale/' % PROJECTNAME)
--- voteit/core/widget.py
+++ voteit/core/widget.py
@@ -1,8 +1,9 @@
-from deform.widget import RadioChoiceWidget
+import os
 
+from deform.widget import RadioChoiceWidget
+from deform.widget import default_resource_registry
 from pkg_resources import resource_filename
 from deform import Form
-import os
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__).decode('utf-8'))
 search_path = (os.path.join(CURRENT_PATH, 'views/templates/widgets'), resource_filename('deform', 'templates'))
@@ -10,15 +11,9 @@ Form.set_zpt_renderer(search_path)
 
 class StarWidget(RadioChoiceWidget):
     #FIXME: the resources for this widget is now hardcoded into main.pt, they should be added through deform resource manager
-#    requirements = ( ('jquery.rating', None), )
+    #requirements = ( ('jquery.rating', None), )
     template = 'star_choice'
     readonly_template = 'readonly/star_choice'
     
-resources = {
-    'jquery.rating': {
-        None:{
-            'js':'scripts/jquery.rating.js',
-            'css':'css/jquery.rating.css',
-            },
-        },
-}
+#default_resource_registry.set_js_resources('jquery.rating', None, 'scripts/jquery.rating.js')
+#default_resource_registry.set_css_resources('jquery.rating', None, 'css/jquery.rating.css')
