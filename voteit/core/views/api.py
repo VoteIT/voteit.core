118c8391a17fd813e6e605811078041d06193175
Robin Harms Oredsson <robin@betahaus.net>
2011-09-26 09:32:14 +0200

    Notify people who view a meeting when then meeting isn't ongoing

--- voteit/core/views/api.py
+++ voteit/core/views/api.py
@@ -127,6 +127,13 @@ class APIView(object):
         """ Is the current context inside a meeting, or a meeting itself? """
         return find_interface(self.context, IMeeting)
 
+    @reify
+    def meeting_state(self):
+        """ Current wf state of meeting if you're inside a meeting. """
+        if not self.meeting:
+            return
+        return self.meeting.get_workflow_state()
+
     def get_navigation(self):
         """ Get navigatoin """
 
@@ -277,4 +284,3 @@ class APIView(object):
     def get_metadata_for_query(self, **kwargs):
         """ Return metadata for catalog search result. """
         return metadata_for_query(self.root.catalog, **kwargs)
-
