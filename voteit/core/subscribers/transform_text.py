5172e400d6bf4b2d5579a2ecd6fcd3afdde457ed
Fredrik Nilson <fredrik@betahaus.net>
2011-09-26 14:16:56 +0200

    adding testt for transform text subscriber

--- voteit/core/subscribers/transform_text.py
+++ voteit/core/subscribers/transform_text.py
@@ -9,6 +9,7 @@ from pyramid.traversal import find_root
 from repoze.folder.interfaces import IObjectAddedEvent
 from webhelpers.html.converters import nl2br
 
+from voteit.core.models.interfaces import ISiteRoot
 from voteit.core.models.interfaces import IProposal
 from voteit.core.models.interfaces import IDiscussionPost
 from voteit.core.interfaces import IObjectUpdatedEvent
@@ -20,7 +21,9 @@ def urls_to_links(text):
     
     return text
 
-def at_userid_link(text, users):
+def at_userid_link(text, obj):
+    root = find_root(obj)
+    users = root.users
     request = get_current_request()
 
     regexp = r'(\A|\s)@('+userid_regexp+r')'
@@ -40,26 +43,22 @@ def at_userid_link(text, users):
 
 @subscriber([IProposal, IObjectAddedEvent])
 def transform_title(obj, event):
-    root = find_root(obj)
-
     text = obj.get_field_value('title')
 
     # nl2br returns an object that automagicaly escapes html, so we converts it to a unicode string
     text = unicode(nl2br(text))
     text = urls_to_links(text)
-    text = at_userid_link(text, root.users)
+    text = at_userid_link(text, obj)
 
     obj.set_field_value('title', text)
 
 @subscriber([IDiscussionPost, IObjectAddedEvent])
 def transform_text(obj, event):
-    root = find_root(obj)
-    
     text = obj.get_field_value('text')
 
     # nl2br returns an object that automagicaly escapes html, so we converts it to a unicode string
     text = unicode(nl2br(text))
     text = urls_to_links(text)
-    text = at_userid_link(text, root.users)
+    text = at_userid_link(text, obj)
 
     obj.set_field_value('text', text)
