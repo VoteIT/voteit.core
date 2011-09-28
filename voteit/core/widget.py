f10ee810a0df194b0266c8b77758a2b1232699b2
Fredrik Nilson <fredrik@betahaus.net>
2011-09-28 11:30:45 +0200

    lets every body create meeting adding captcha to meeting creation

--- voteit/core/widget.py
+++
@@ -1,8 +0,0 @@
-from deform.widget import RadioChoiceWidget
-
-
-class StarWidget(RadioChoiceWidget):
-    #FIXME: the resources for this widget is now hardcoded into main.pt, they should be added through deform resource manager
-    #requirements = ( ('jquery.rating', None), )
-    template = 'star_choice'
-    readonly_template = 'readonly/star_choice'
---
+++ voteit/core/widgets.py
@@ -0,0 +1,73 @@
+import httplib2
+from urllib import urlencode
+
+import colander
+from colander import null
+from colander import Invalid
+
+from deform.widget import CheckedInputWidget
+from deform.widget import RadioChoiceWidget
+
+from pyramid.settings import get_settings
+from pyramid.threadlocal import get_current_request
+
+class StarWidget(RadioChoiceWidget):
+    #FIXME: the resources for this widget is now hardcoded into main.pt, they should be added through deform resource manager
+    #requirements = ( ('jquery.rating', None), )
+    template = 'star_choice'
+    readonly_template = 'readonly/star_choice'
+
+class RecaptchaWidget(CheckedInputWidget):
+    template = 'captcha'
+    readonly_template = 'captcha'
+    requirements = ()
+    url = "http://www.google.com/recaptcha/api/verify"
+    headers={'Content-type': 'application/x-www-form-urlencoded'}
+
+    def serialize(self, field, cstruct, readonly=False):
+        if cstruct in (null, None):
+            cstruct = ''
+        confirm = getattr(field, 'confirm', '')
+        template = readonly and self.readonly_template or self.template
+        return field.renderer(template, field=field, cstruct=cstruct,
+                              public_key=get_settings()['captcha_public_key'],
+                              )
+
+    def deserialize(self, field, pstruct):
+        if pstruct is null:
+            return null
+        challenge = pstruct.get('recaptcha_challenge_field') or ''
+        response = pstruct.get('recaptcha_response_field') or ''
+        if not response:
+            raise Invalid(field.schema, 'No input')
+        if not challenge:
+            raise Invalid(field.schema, 'Missing challenge')
+        privatekey = get_settings()['captcha_private_key']
+        request = get_current_request()
+        remoteip = request.remote_addr
+        data = urlencode(dict(privatekey=privatekey,
+                              remoteip=remoteip,
+                              challenge=challenge,
+                              response=response))
+        h = httplib2.Http(timeout=10)
+        try:
+            resp, content = h.request(self.url,
+                                      "POST",
+                                      headers=self.headers,
+                                      body=data)
+        except AttributeError as e:
+            if e=="'NoneType' object has no attribute 'makefile'":
+                ## XXX: catch a possible httplib regression in 2.7 where
+                ## XXX: there is no connextion made to the socker so
+                ## XXX sock is still None when makefile is called.
+                raise Invalid(field.schema,
+                              "Could not connect to the captcha service.")
+        if not resp['status'] == '200':
+            raise Invalid(field.schema,
+                          "There was an error talking to the recaptcha \
+                          server{0}".format(resp['status']))
+        valid, reason = content.split('\n')
+        if not valid == 'true':
+            if reason == 'incorrect-captcha-sol':
+                reason = "Incorrect solution"
+            raise Invalid(field.schema, reason.replace('\\n', ' ').strip("'") )
