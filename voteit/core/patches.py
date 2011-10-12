487d769f87022a05146eceb00a687bb3564c1c49
Robin Harms Oredsson <robin@betahaus.net>
2011-10-12 12:15:56 +0200

    Enabling override of radio_choice widget

--- voteit/core/patches.py
+++ voteit/core/patches.py
@@ -1,9 +1,14 @@
-""" Contains things that should be loaded on startup. """
+""" Contains things that should be loaded on startup.
+    This file should be removed as soon as we can register things in a smarter way,
+    or persuade the deform gang to include hooks for this kind of stuff :)
+"""
 
-import os
+from os.path import join
 from pkg_resources import resource_filename
 
 from deform import Form
+from deform.widget import RadioChoiceWidget
+
 from pyramid.i18n import get_localizer
 from pyramid.threadlocal import get_current_request
 
@@ -13,7 +18,11 @@ def translator(term):
 
 
 CURRENT_PATH = resource_filename('voteit.core', '')
-search_path = (os.path.join(CURRENT_PATH, 'views', 'templates', 'widgets'),
+WIDGET_PATH = join(CURRENT_PATH, 'views', 'templates', 'widgets')
+search_path = (WIDGET_PATH,
                resource_filename('deform', 'templates'))
 
 Form.set_zpt_renderer(search_path, translator=translator)
+
+#Patch radio choice widget to use a sane readonly template
+RadioChoiceWidget.readonly_template = join(WIDGET_PATH, 'readonly', 'radio_choice')
