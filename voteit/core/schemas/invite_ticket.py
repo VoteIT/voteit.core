78b275b3863cddf7206cab71b131521a39e0bef0
Robin Harms Oredsson <robin@betahaus.net>
2011-10-13 11:58:35 +0200

    Updated ticket description texts and made normal participant default option when inviting

--- voteit/core/schemas/invite_ticket.py
+++ voteit/core/schemas/invite_ticket.py
@@ -15,8 +15,8 @@ class ClaimTicketSchema(colander.Schema):
                                 validator = colander.Email(msg = _(u"Invalid email address.")),)
     #Note that validation for token depends on email, so it can't be set at field level.
     token = colander.SchemaNode(colander.String(),
-                                title = _(u"The access token your received in your email."),)
-
+                                title = _(u"claim_ticket_token_title",
+                                          default = u"The access token your received in your email."),)
 
             
 @schema_factory('AddTicketsSchema')
@@ -24,14 +24,20 @@ class AddTicketsSchema(colander.Schema):
     roles = colander.SchemaNode(
         deform.Set(),
         title = _(u"Roles"),
-        description = _(u'One user can have more than one role. Note that to be able to propose, discuss and vote the users needs to be participant AND voter.'),
+        default = (security.ROLE_DISCUSS, security.ROLE_PROPOSE, security.ROLE_VOTER),
+        description = _(u"add_tickets_roles_description",
+                        default = u"""One user can have more than one role. Note that to be able to propose,
+                        discuss and vote you need respective role. This is selected by default. If you want
+                        to add a user that can only view, select View and uncheck everything else."""),
         widget = deform.widget.CheckboxChoiceWidget(values=security.MEETING_ROLES,),
     )
     #FIXME: Validate that an invite doesn't already exist.
     #FIXME: Add custom subject line. No sense in having it static!
     emails = colander.SchemaNode(colander.String(),
-                                 title = _(u"Email addresses to give the role above."),
-                                 description = _(u'Separate email adresses with a single line break'),
+                                 title = _(u"add_tickets_emails_titles",
+                                           default=u"Email addresses to give the roles above."),
+                                 description = _(u"add_tickets_emails_description",
+                                                 default=u'Separate email addresses with a single line break.'),
                                  widget = deform.widget.TextAreaWidget(rows=7, cols=40),
                                  validator = colander.All(html_string_validator, multiple_email_validator),
     )
@@ -41,7 +47,6 @@ class AddTicketsSchema(colander.Schema):
                                   widget = deform.widget.TextAreaWidget(rows=5, cols=40),
                                   default = _('invitation_default_text',
                                               default=u"You've received a meeting invitation for a VoteIT meeting."),
-                                  missing = u'',
                                   validator = html_string_validator,
     )
     
