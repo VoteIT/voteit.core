15658b495c5ff2c31b15055791236586217dbbdc
Robin Harms Oredsson <robin@betahaus.net>
2011-10-17 13:11:56 +0200

    javascript config file isn't static + fixed inheritance. It shouldn't inherit baseview, since it will initialize and run a lot of stuff in the api class.

--- voteit/core/views/javascript.py
+++ voteit/core/views/javascript.py
@@ -1,14 +1,11 @@
 from pyramid.view import view_config
 
 from voteit.core.models.interfaces import ISiteRoot
-from voteit.core.views.base_view import BaseView
 from voteit.core.security import VIEW
 
 
-class JavaScript(BaseView):
-    
-    @view_config(context=ISiteRoot, name="config.js", permission=VIEW, renderer='../static/config.js.pt')
-    def javascript_config(self):
-        #Note that self.request.response is not the same as self.response which is our dict
-        self.request.response.content_type = 'text/javascript;;charset=utf-8'
-        return self.response
+@view_config(context=ISiteRoot, name="config.js", permission=VIEW, renderer='templates/snippets/config.js.pt')
+def javascript_config(request):
+    #Note that self.request.response is not the same as self.response which is our dict
+    request.response.content_type = 'text/javascript;;charset=utf-8'
+    return {}
