f8533fa1037141b7516bfc5afa4a589a9f3ee43c
Robin Harms Oredsson <robin@betahaus.net>
2011-10-18 14:11:12 +0200

    poll_plugin should provide at least some defaults corresponding to IPollPlugin

--- voteit/core/models/poll_plugin.py
+++ voteit/core/models/poll_plugin.py
@@ -3,6 +3,7 @@ from zope.component import adapts
 
 from voteit.core.models.interfaces import IPoll
 from voteit.core.models.interfaces import IPollPlugin
+from voteit.core.models.vote import Vote
 
 
 class PollPlugin(object):
@@ -12,6 +13,62 @@ class PollPlugin(object):
     """
     implements(IPollPlugin)
     adapts(IPoll)
+    name = 'BasePollPlugin'
+    title = 'BasePollPlugin'
+    description = 'Subclass this to make a usable PollPlugin'
     
     def __init__(self, context):
         self.context = context
+
+    def get_vote_schema(self):
+        """ Return the schema of how a vote should be structured.
+            This is used to render a voting form.
+        """
+        raise NotImplementedError("Must be provided by subclass")
+    
+    def get_vote_class(self):
+        """ Get the vote class to use for this poll. Normally it's the
+            voteit.core.models.vote.Vote class.
+        """
+        return Vote
+
+    def get_settings_schema(self):
+        """ Get an instance of the schema used to render a form for editing settings.
+            If this is None, this poll method doesn't have any settings.
+        """
+        return None
+
+    def handle_close(self):
+        """ Handle closing of the poll.
+        """
+        raise NotImplementedError("Must be provided by subclass")
+
+    def render_result(self, request):
+        """ Return rendered html with result display. Called by the poll view
+            when the poll has finished.
+        """
+        raise NotImplementedError("Must be provided by subclass")
+
+    def change_states_of(self):
+        """ This gets called when a poll has finished.
+            It returns a dictionary with proposal uid as key and new state as value.
+            Like: {'<uid>':'approved', '<uid>', 'denied'}
+            It's not required to do, but if it isn't done, the proposals won't change state
+            and you have to do it manually.
+            It's not required to return anything.
+        """
+        return {}
+
+    def render_raw_data(self):
+        """ Return rendered html with raw data from this poll.
+            It should consist of ballot information.
+            It can either be anonymous, or actually show the userids of the ones
+            that voted. That's just a matter of taste.
+            The point with this view is to enable others to run
+            a poll to verify the result.
+            
+            The method needs to return an instance of either:
+            - pyramid.renderers.render
+            - pyramid.response.Response
+        """
+        raise NotImplementedError("Must be provided by subclass")
--- voteit/core/models/tests/test_poll_plugin.py
+++ voteit/core/models/tests/test_poll_plugin.py
@@ -2,17 +2,15 @@ import unittest
 
 from pyramid import testing
 from pyramid.threadlocal import get_current_registry
+from zope.interface.verify import verifyClass
 from zope.interface.verify import verifyObject
 from zope.interface.exceptions import BrokenImplementation
+from zope.interface.exceptions import BrokenMethodImplementation
+from zope.interface.exceptions import DoesNotImplement
 from zope.component import queryUtility
+from zope.interface import implements
 
-
-def _bad_mock_plugin_class():
-    """ Get a mock plugin class that doesn't provide the proper attributes. """
-    from voteit.core.models.poll_plugin import PollPlugin
-    class BadPlugin(PollPlugin):
-        pass
-    return BadPlugin
+from voteit.core.models.interfaces import IPollPlugin
 
     
 class PollPluginTests(unittest.TestCase):
@@ -21,32 +19,52 @@ class PollPluginTests(unittest.TestCase):
 
     def tearDown(self):
         testing.tearDown()
-    
+
+    @property
+    def _cut(self):
+        from voteit.core.models.poll_plugin import PollPlugin
+        return PollPlugin
+
     def _example_plugin_class(self):
         from voteit.core.plugins.majority_poll import MajorityPollPlugin
         return MajorityPollPlugin
+    
+    def _bad_method_plugin(self):
+        class _BadMethodPlugin(self._cut):
+            def get_vote_schema(self, to, many, arguments):
+                pass
+        return _BadMethodPlugin
 
-    def test_register_bad_plugin(self):
-        """ Check that registration of a bad plugin raises errors.
-        """
+    def test_verify_class(self):
+        self.failUnless(verifyClass(IPollPlugin, self._cut))
+
+    def test_verify_obj(self):
+        context = testing.DummyModel()
+        self.failUnless(verifyObject(IPollPlugin, self._cut(context)))
+
+    def test_register_bad_method_plugin(self):
         from voteit.core.app import register_poll_plugin
-        bad_cls = _bad_mock_plugin_class()
-        
-        self.assertRaises(BrokenImplementation, register_poll_plugin, bad_cls)
+        self.assertRaises(BrokenMethodImplementation, register_poll_plugin, self._bad_method_plugin())
+
+    def test_register_bad_plugin_not_implemented(self):
+        from voteit.core.app import register_poll_plugin
+        self.assertRaises(DoesNotImplement, register_poll_plugin, object)
+
+    def test_register_bad_plugin_wrong_attrs(self):
+        from voteit.core.app import register_poll_plugin
+        class _BadPlugin(object):
+            implements(IPollPlugin)
+        self.assertRaises(BrokenImplementation, register_poll_plugin, _BadPlugin)
 
     def test_good_plugin(self):
         """ Register example plugin that ships with voteit. """ 
         from voteit.core.app import register_poll_plugin
-        from voteit.core.models.interfaces import IPollPlugin
         from voteit.core.models.poll import Poll
         
         registry = get_current_registry()
         good_cls = self._example_plugin_class()
         register_poll_plugin(good_cls, verify=0, registry=registry)
         name = good_cls.name
-        
         poll = Poll()
-
         poll_plugin = registry.getAdapter(poll, name = name, interface = IPollPlugin)
-
         self.failUnless( verifyObject(IPollPlugin, poll_plugin) )
