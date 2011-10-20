f7127f84652ba8d7cf17430efaa0bac3e0119494
Robin Harms Oredsson <robin@betahaus.net>
2011-10-20 14:08:29 +0200

    Iface docs for export / import

--- voteit/core/models/interfaces.py
+++ voteit/core/models/interfaces.py
@@ -439,7 +439,25 @@ class IExportImport(Interface):
     """
     def __init__(context):
         """ Object to adapt """
+    def export_buffer(obj):
+        """ Returns filebuffer created from object to export.
+        """
 
+    def download_export(obj):
+        """ Returns a Response object with an export file.
+            It's ment to be passed to the client as a response, and will initiate download.
+            The Response.body attr should contain everything from export_buffer method.
+        """
+
+    def import_data(parent, name, filedata):
+        """ Import data into site, and index the new objects in the catalog.
+            parent
+                Object where the new data will be stored
+            name
+                Which key the new data will be stored under, ie parent[name]
+            filedata
+                Contains a filebuffer, like an open file stream.
+        """
 
 #Marker interfaces
 class ICatalogMetadataEnabled(Interface):
