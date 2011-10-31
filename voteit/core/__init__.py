30e76c557fcc69bc0f04570aad15dbdfd4ed2ec3
Robin Harms Oredsson <robin@betahaus.net>
2011-10-31 15:44:52 +0100

    Let's just use one ting for plugins: plugins

--- voteit/core/__init__.py
+++ voteit/core/__init__.py
@@ -20,7 +20,6 @@ def main(global_config, **settings):
     import voteit.core.patches
     config = default_configurator(settings)
     config.include(required_components)
-    config.include(register_plugins)
     config.hook_zca()
     return config.make_wsgi_app()
 
@@ -49,16 +48,9 @@ def required_components(config):
     #For password storage
     config.scan('betahaus.pyracont.fields.password')
 
-    #Include all poll plugins from paster .ini config
-    config.include(register_poll_plugins)
-
     cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
     config.add_static_view('static', '%s:static' % PROJECTNAME, cache_max_age = cache_ttl_seconds)
     config.add_static_view('deform', 'deform:static', cache_max_age = cache_ttl_seconds)
-
-    #Set which mailer to use
-    config.include(config.registry.settings['mailer'])
-
     config.add_translation_dirs('deform:locale/',
                                 'colander:locale/',
                                 '%s:locale/' % PROJECTNAME,)
@@ -67,18 +59,7 @@ def required_components(config):
     config.include(adjust_view_component_order)
     from voteit.core.security import VIEW
     config.set_default_permission(VIEW)    
-
-
-def register_poll_plugins(config):
-    """ Register poll plugins configured in the paster .ini file.
-        At least one must be used, otherwise polls won't work.
-    """
-    poll_plugins = config.registry.settings.get('poll_plugins')
-    if poll_plugins is not None:
-        for poll_plugin in poll_plugins.strip().splitlines():
-            config.include(poll_plugin)
-    else:
-        raise ValueError("At least one poll plugin must be used")
+    config.include(register_plugins)
 
 
 def register_plugins(config):
