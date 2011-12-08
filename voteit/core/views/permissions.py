f01369f01f63f1e80e2a180651c97e94dd6a7b36
Fredrik Nilson <fredrik@betahaus.net>
2011-12-08 13:52:21 +0100

    more usefull permission edit view for meeting

--- voteit/core/views/permissions.py
+++ voteit/core/views/permissions.py
@@ -9,15 +9,14 @@ from voteit.core.views.base_view import BaseView
 from voteit.core.models.interfaces import IMeeting
 from voteit.core.models.interfaces import ISiteRoot
 from voteit.core.models.schemas import add_csrf_token
-from voteit.core.security import MANAGE_GROUPS
+from voteit.core import security
 
 
 class PermissionsView(BaseView):
     """ View for setting permissions """
 
-    @view_config(context=IMeeting, name="permissions", renderer="templates/base_edit.pt", permission=MANAGE_GROUPS)
-    @view_config(context=ISiteRoot, name="permissions", renderer="templates/base_edit.pt", permission=MANAGE_GROUPS)
-    def meeting_group_form(self):
+    @view_config(context=ISiteRoot, name="permissions", renderer="templates/base_edit.pt", permission=security.MANAGE_GROUPS)
+    def root_group_form(self):
         post = self.request.POST
         if 'cancel' in post:
             url = resource_url(self.context, self.request)
@@ -37,6 +36,7 @@ class PermissionsView(BaseView):
                 self.response['form'] = e.render()
                 return self.response
             
+            import pdb; pdb.set_trace()
             #Set permissions
             self.context.set_security(appstruct['userids_and_groups'])
             url = resource_url(self.context, self.request)
@@ -48,4 +48,47 @@ class PermissionsView(BaseView):
         )
 
         self.response['form'] = form.render(appstruct=appstruct)
+        return self.response
+
+    @view_config(context=IMeeting, name="permissions", renderer="templates/meeting_permissions.pt", permission=security.MANAGE_GROUPS)
+    def meeting_group_form(self):
+        post = self.request.POST
+        if 'cancel' in post:
+            url = resource_url(self.context, self.request)
+            return HTTPFound(location=url)
+
+        if 'save' in post:
+            userids_and_groups = []
+            controls = post.items()
+            for control in controls:
+                if '__start__' in control[0]:
+                    userid = control[1]
+                    groups = []
+                if 'group' in control[0]:
+                    groups.append(control[1])
+                if '__end__' in control[0]:
+                    userids_and_groups.append({'userid': userid, 'groups': set(groups)})
+
+            #Set permissions
+            self.context.set_security(userids_and_groups)
+            url = self.request.path_url
+            return HTTPFound(location=url)
+
+        #No action - Render edit form
+        
+        users = self.api.root.users
+        
+        results = []
+        for userid in security.find_authorized_userids(self.context, (security.VIEW,)):
+            user = users.get(userid, None)
+            if user:
+                results.append(user)
+        
+        def _sorter(obj):
+            return obj.get_field_value('first_name')
+
+        #Viewer role isn't needed, since only users who can view will be listed here.
+        self.response['roles'] = security.MEETING_ROLES
+        self.response['participants'] = tuple(sorted(results, key = _sorter))
+        self.response['context_effective_principals'] = security.context_effective_principals
         return self.response
