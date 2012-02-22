38409414660cf2e26d64e9580555ac7c31bf1e7a
robinharms <robin@betahaus.net>
2012-02-22 11:10:16 +0100

    Menu alternative for minutes

--- voteit/core/views/components/meeting_actions.py
+++ voteit/core/views/components/meeting_actions.py
@@ -103,6 +103,7 @@ def generic_root_menu_link(context, request, va, **kw):
 @view_action('settings_menu', 'access_policye', title = _(u"Access policy"), link = "@@access_policy", permission = EDIT, )
 @view_action('settings_menu', 'mail_settings', title = _(u"Mail settings"), link = "@@mail_settings", permission = EDIT, )
 @view_action('settings_menu', 'presentation', title = _(u"Presentation"), link = "@@presentation", permission = EDIT, )
+@view_action('settings_menu', 'minutes', title = _(u"Minutes"), link = "@@minutes", permission = MODERATE_MEETING, )
 @view_action('participants_menu', 'participants_emails', title = _(u"Participants email addresses"), link = "@@participants_emails", permission = MODERATE_MEETING, )
 @view_action('participants_menu', 'permissions', title = _(u"Edit permissions"), link = "@@permissions", permission = MANAGE_GROUPS, )
 @view_action('participants_menu', 'manage_tickets', title = _(u"Manage invites"), link = "@@manage_tickets", permission = MANAGE_GROUPS, )
