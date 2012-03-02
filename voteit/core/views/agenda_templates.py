75ff0dbc6cd6a8b9065aba21f2836ff8ca644879
Fredrik Nilson <fredrik@betahaus.net>
2012-03-02 13:41:40 +0100

    views and templates for choosing an agenda template and populating the meeting

--- voteit/core/views/agenda_templates.py
+++ voteit/core/views/agenda_templates.py
@@ -4,21 +4,51 @@ from deform.exception import ValidationFailure
 from pyramid.url import resource_url
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
+from pyramid.traversal import find_root
 
 from voteit.core import VoteITMF as _
 from voteit.core.views.base_view import BaseView
 from voteit.core.models.interfaces import IAgendaTemplates
 from voteit.core.models.interfaces import IAgendaTemplate
+from voteit.core.models.interfaces import IMeeting
 from voteit.core import security
 
 
-class PermissionsView(BaseView):
-    """ View for setting permissions """
+class AgendaTempalteView(BaseView):
+    """ View for Agenda tempaltes """
     
     @view_config(context=IAgendaTemplates, renderer="templates/agenda_templates.pt", permission=security.VIEW)
     def agenda_templates(self):
+        self.response['agenda_templates'] = self.context
+        
         return self.response
     
     @view_config(context=IAgendaTemplate, renderer="templates/agenda_template.pt", permission=security.VIEW)
     def agenda_template(self):
+        return self.response
+    
+    @view_config(context=IMeeting, name="agenda_templates", renderer="templates/agenda_templates.pt", permission=security.EDIT)
+    def agenda_template_select(self):
+        agenda_templates = self.api.root['agenda_templates']
+        
+        get = self.request.GET
+        if 'apply' in get:
+            template_name = get['apply']
+            if template_name in agenda_templates:
+                template = agenda_templates[template_name]
+                template.populate_meeting(self.context)
+                
+                msg = _(u"agenda_template_apply_template_success",
+                        default = u"Selected template applied to meeting.")
+                self.api.flash_messages.add(msg)
+                
+                return HTTPFound(location = resource_url(self.context, self.request))
+            else:
+                err_msg = _(u"agenda_template_apply_invalid_template",
+                        default = u"No template named ${template} could be found.",
+                        mapping = {'template': template_name})
+                self.api.flash_messages.add(err_msg, type="error")
+        
+        self.response['agenda_templates'] = agenda_templates
+        
         return self.response
