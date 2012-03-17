3c977e8b1cb44911f6b338b803bdcea4f6b3d9d8
Anders Hultman <anders@betahaus.net>
2012-03-16 22:44:16 -0400

    Corrections and more text in RSS feed and truncate

--- voteit/core/schemas/meeting.py
+++ voteit/core/schemas/meeting.py
@@ -49,7 +49,11 @@ def meeting_mail_address_node():
     
 def rss_feed_node():
     return colander.SchemaNode(colander.Boolean(),
-                               title = _(u"RSS feed available"),
+                               title = _(u"Activate RSS feed"),
+								description = _(u"rss_feed_checkbox_description",
+								default=u"When the checkbox below is checked your meeting will be able to show a public RSS feed that can be followed with a RSS reader. This feed will contain info about when changes are made in the meeting and who did the changes. You can access the feed on: 'The meeting URL' + '/feed'. This should be something like 'www.yourdomain.com/yourmeetingname/feed'. If you want the feed to show up in an iframe you can use '/framefeed' instead. This is an advanced feature and read more about it in the manual on wiki.voteit.se. Please note a word of warning: the feed is public for all who can figure it out."),
+
+
                                default = False,)
 
 def access_policy_node():
