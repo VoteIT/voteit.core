bf8791a33080c50cb9c84d88eb3cbaf02c97dce2
robinharms <robin@betahaus.net>
2012-04-04 13:41:38 +0200

    Tests need to be checked and run after written. Otherwise they're pointless...

--- voteit/core/subscribers/tests/test_agenda_item.py
+++ voteit/core/subscribers/tests/test_agenda_item.py
@@ -7,27 +7,22 @@ from voteit.core.events import ObjectUpdatedEvent
 from voteit.core.testing_helpers import register_workflows
 
 
-class PollTests(unittest.TestCase):
+class AgendaItemSubscriberTests(unittest.TestCase):
     def setUp(self):
-        self.config = testing.setUp()
+        request = testing.DummyRequest()
+        self.config = testing.setUp(request = request)
         self.config.scan('voteit.core.subscribers.agenda_item')
 
     def tearDown(self):
         testing.tearDown()
 
     def test_change_states_proposals(self):
-        request = testing.DummyRequest()
-        self.config = testing.setUp(registry = self.config.registry, request = request)
-
         from voteit.core.models.meeting import Meeting
         from voteit.core.models.agenda_item import AgendaItem
         meeting = Meeting()
-        
         ai = AgendaItem()
         meeting['a1'] = ai
-        
         ai = AgendaItem()
         meeting['a2'] = ai
-        
         self.assertEqual(meeting['a1'].get_field_value('order'), 0)
         self.assertEqual(meeting['a2'].get_field_value('order'), 1)
