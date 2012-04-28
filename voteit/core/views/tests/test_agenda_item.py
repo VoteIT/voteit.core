fe8f4c0e12e089e0c4478831affa466c9f3c0fd9
robinharms <robin@betahaus.net>
2012-04-28 16:23:31 +0200

    Proper coverage of agenda item views

--- voteit/core/views/tests/test_agenda_item.py
+++ voteit/core/views/tests/test_agenda_item.py
@@ -2,8 +2,10 @@ import datetime
 import unittest
 
 from pyramid import testing
+from pyramid.httpexceptions import HTTPForbidden
 
 from voteit.core.testing_helpers import bootstrap_and_fixture
+from voteit.core.testing_helpers import register_security_policies
 
 
 class AgendaItemViewTests(unittest.TestCase):
@@ -83,43 +85,72 @@ class AgendaItemViewTests(unittest.TestCase):
     def test_inline_add_form_proposal(self):
         self.config.testing_securitypolicy(userid='dummy',
                                            permissive=True)
-        
         self.config.scan('voteit.core.models.proposal')
         self.config.scan('voteit.core.schemas.proposal')
-        
         context = self._fixture()
         request = testing.DummyRequest(params={'content_type': 'Proposal'})
-         
         aiv = self._cut(context, request)
         response = aiv.inline_add_form()
-
         self.assertIn(u"content_type=Proposal", response.ubody)
-        
+
+    def test_inline_add_form_no_permission(self):
+        register_security_policies(self.config)
+        self.config.scan('voteit.core.models.proposal')
+        self.config.scan('voteit.core.schemas.proposal')
+        context = self._fixture()
+        request = testing.DummyRequest(params={'content_type': 'Proposal'})
+        obj = self._cut(context, request)
+        self.assertRaises(HTTPForbidden, obj.inline_add_form)
+
     def test_inline_add_form_discussion_post(self):
         self.config.testing_securitypolicy(userid='dummy',
                                            permissive=True)
-        
         self.config.scan('voteit.core.models.discussion_post')
         self.config.scan('voteit.core.schemas.discussion_post')
-        
         context = self._fixture()
         request = testing.DummyRequest(params={'content_type': 'DiscussionPost'})
-         
         aiv = self._cut(context, request)
         response = aiv.inline_add_form()
-
         self.assertIn(u"content_type=DiscussionPost", response.ubody)
         
     def test_discussion_more(self):
         self.config.testing_securitypolicy(userid='dummy',
                                            permissive=True)
-        
         from voteit.core.models.discussion_post import DiscussionPost
         context = DiscussionPost() 
         context.title = "Testing read more view"
-        
         request = testing.DummyRequest()
         aiv = self._cut(context, request)
         response = aiv.discussion_more()
-        
         self.assertEqual(response['body'], context.title)
+
+    def test_discussions_no_ajax(self):
+        self.config.scan('voteit.core.views.components.discussions')
+        self.config.testing_securitypolicy(userid='dummy',
+                                           permissive=True)
+        context = self._fixture()
+        request = testing.DummyRequest(is_xhr = False)
+        obj = self._cut(context, request)
+        res = obj.discussions()
+        self.assertEqual(res.location, 'http://example.com/m/ai/#discussions')
+
+    def test_discussions_no_ajax_load_all(self):
+        self.config.scan('voteit.core.views.components.discussions')
+        self.config.testing_securitypolicy(userid='dummy',
+                                           permissive=True)
+        context = self._fixture()
+        request = testing.DummyRequest(is_xhr = False,
+                                       params = {'discussions': '1'})
+        obj = self._cut(context, request)
+        res = obj.discussions()
+        self.assertEqual(res.location, 'http://example.com/m/ai/?discussions=all#discussions')
+
+    def test_discussions_w_ajax(self):
+        self.config.scan('voteit.core.views.components.discussions')
+        self.config.testing_securitypolicy(userid='dummy',
+                                           permissive=True)
+        context = self._fixture()
+        request = testing.DummyRequest(is_xhr = True)
+        obj = self._cut(context, request)
+        res = obj.discussions()
+        self.assertEqual(res.status, '200 OK')
