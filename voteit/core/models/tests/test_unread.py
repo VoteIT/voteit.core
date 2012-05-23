f9483d8aca5e893c91ab68cc4edf17ab4dbf42dc
robinharms <robin@betahaus.net>
2012-05-23 12:13:23 +0200

    Unread tests stopped working when we fixed perms for private

--- voteit/core/models/tests/test_unread.py
+++ voteit/core/models/tests/test_unread.py
@@ -13,6 +13,9 @@ from voteit.core.testing_helpers import register_security_policies
 from voteit.core.models.interfaces import IUnread
 
 
+
+
+
 class UnreadTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
@@ -22,41 +25,35 @@ class UnreadTests(unittest.TestCase):
 
     def _fixture_and_setup(self):
         root = bootstrap_and_fixture(self.config)
-
         from voteit.core.models.user import User
-        
+        from voteit.core.models.agenda_item import AgendaItem
+        #from voteit.core.models.meeting import Meeting
+        from voteit.core.models.proposal import Proposal
         for userid in ('fredrik', 'anders', 'hanna', 'robin'):
             root.users[userid] = User()
-
-        return root
+        root['ai'] = AgendaItem()
+        security.unrestricted_wf_transition_to(root['ai'], 'upcoming')
+        root['ai']['p'] = Proposal()
+        return root['ai']['p']
 
     @property
-    def _fut(self):
+    def _cut(self):
         from voteit.core.models.unread import Unread
         return Unread
-    
-    def _make_proposal(self):
-        """ Since it also implements UnreadAware and has required security mixin. """
-        from voteit.core.models.proposal import Proposal
-        return Proposal()
 
     def test_verify_class(self):
-        self.assertTrue(verifyClass(IUnread, self._fut))
+        self.assertTrue(verifyClass(IUnread, self._cut))
 
     def test_verify_obj(self):
         register_security_policies(self.config)
-        root = self._fixture_and_setup()
-        context = self._make_proposal()
-        root['c'] = context
-        obj = self._fut(context)
+        context = self._fixture_and_setup()
+        obj = self._cut(context)
         self.assertTrue(verifyObject(IUnread, obj))
 
     def test_mark_as_read(self):
         register_security_policies(self.config)
-        root = self._fixture_and_setup()
-        context = self._make_proposal()
-        root['c'] = context
-        obj = self._fut(context)
+        context = self._fixture_and_setup()
+        obj = self._cut(context)
         userid = 'somebody'
         obj.unread_storage.add(userid)
         self.assertTrue(userid in obj.get_unread_userids())
@@ -65,30 +62,24 @@ class UnreadTests(unittest.TestCase):
 
     def test_unread_on_check(self):
         register_security_policies(self.config)
-        root = self._fixture_and_setup()
-        prop = self._make_proposal()
-        root['new'] = prop
-        obj = self._fut(prop)
+        context = self._fixture_and_setup()
+        obj = self._cut(context)
         self.assertEqual(obj.get_unread_userids(), frozenset(('admin',)))
 
     def test_only_viewers_added(self):
         register_security_policies(self.config)
-        root = self._fixture_and_setup()
-        prop = self._make_proposal()
-        root['new'] = prop
-        prop.add_groups('hanna', [security.ROLE_VIEWER])
-        prop.add_groups('anders', [security.ROLE_VIEWER])
-        obj = self._fut(prop)
+        context = self._fixture_and_setup()
+        context.add_groups('hanna', [security.ROLE_VIEWER])
+        context.add_groups('anders', [security.ROLE_VIEWER])
+        obj = self._cut(context)
         self.assertEqual(obj.get_unread_userids(), frozenset(('admin', 'hanna', 'anders')))
 
     def test_reset_unread(self):
         register_security_policies(self.config)
-        root = self._fixture_and_setup()
-        prop = self._make_proposal()
-        root['new'] = prop
-        prop.add_groups('hanna', [security.ROLE_VIEWER])
-        prop.add_groups('anders', [security.ROLE_VIEWER])
-        obj = self._fut(prop)
+        context = self._fixture_and_setup()
+        context.add_groups('hanna', [security.ROLE_VIEWER])
+        context.add_groups('anders', [security.ROLE_VIEWER])
+        obj = self._cut(context)
         obj.mark_as_read('hanna')
         obj.reset_unread()
         self.assertEqual(obj.get_unread_userids(), frozenset(('admin', 'hanna', 'anders')))
