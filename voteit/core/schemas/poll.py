946a8b7c8f11deb64a9d00f373472372be9ad911
robinharms <robin@betahaus.net>
2012-06-04 14:37:43 +0200

    Syntax different for new deform/colander datetime widget

--- voteit/core/schemas/poll.py
+++ voteit/core/schemas/poll.py
@@ -126,7 +126,8 @@ class PollSchema(colander.MappingSchema):
          TZDateTime(),
          title = _(u"Start time of this poll."),
          description = _(u"You need to open it yourself."),
-         widget=deform.widget.DateTimeInputWidget(options={'timeFormat': 'hh:mm'}),
+         widget=deform.widget.DateTimeInputWidget(options={'timeFormat': 'hh:mm',
+                                                           'separator': ' '}),
          default = deferred_default_start_time,
     )
     end_time = colander.SchemaNode(
@@ -134,7 +135,8 @@ class PollSchema(colander.MappingSchema):
          title = _(u"End time of this poll."),
          description = _(u"poll_end_time_description",
                          default = u"You need to close it yourself. A good default value is one day later."),
-         widget=deform.widget.DateTimeInputWidget(options={'timeFormat': 'hh:mm'}),
+         widget=deform.widget.DateTimeInputWidget(options={'timeFormat': 'hh:mm',
+                                                           'separator': ' '}),
          default = deferred_default_end_time,
     )
     
