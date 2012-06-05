dcb89466c1a80c7a33db95cc51de72a772eea3a2
Fredrik Nilson <fredrik@betahaus.net>
2012-06-05 11:42:13 +0200

    removing html links before editing discussion posts and proposals

--- voteit/core/schemas/discussion_post.py
+++ voteit/core/schemas/discussion_post.py
@@ -4,6 +4,7 @@ from betahaus.pyracont.decorators import schema_factory
 
 from voteit.core import VoteITMF as _
 from voteit.core.validators import deferred_at_enabled_text
+from voteit.core.widgets import TextAreaStripLinksWidget
 
 
 @schema_factory('DiscussionPostSchema')
@@ -11,4 +12,4 @@ class DiscussionPostSchema(colander.Schema):
     text = colander.SchemaNode(colander.String(),
                                title = _(u"Text"),
                                validator=deferred_at_enabled_text,
-                               widget=deform.widget.TextAreaWidget(rows=3, cols=40),)
+                               widget=TextAreaStripLinksWidget(rows=3, cols=40),)
