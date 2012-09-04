874134a096d2ee76f92d95f3cbf023ca10b39b23
Fredrik Nilson <fredrik@betahaus.net>
2012-09-04 12:19:25 +0200

    wrong count when filtering proposals

--- voteit/core/views/components/proposals.py
+++ voteit/core/views/components/proposals.py
@@ -3,6 +3,7 @@ from pyramid.renderers import render
 from pyramid.traversal import resource_path
 from pyramid.traversal import find_resource
 from repoze.catalog.query import Any
+from repoze.catalog.query import Eq
 from zope.component.interfaces import ComponentLookupError
 
 from voteit.core import VoteITMF as _
@@ -30,54 +31,9 @@ def proposal_listing(context, request, va, **kw):
         response['polls'] = polls
         response['get_proposal_brains'] = _get_proposal_brains
         return render('../templates/polls.pt', response, request=request)
-
-    def _show_retract(brain):
-        #Do more expensive checks last!
-        if brain['workflow_state'] != 'published':
-            return
-        if not api.userid in brain['creators']:
-            return
-        #Now for the 'expensive' stuff
-        obj = find_resource(api.root, brain['path'])
-        return api.context_has_permission(RETRACT, obj)
     
-    # crearting dummy proposal to get state info dict
-    state_info = Proposal().workflow.state_info(None, request)
-    
-    def _translated_state_title(state):
-        for info in state_info:
-            if info['name'] == state:
-                return api.tstring(info['title'])
-        
-        return state
-    
-    query = dict(content_type = 'Proposal',
-                 sort_index = 'created',
-                 reverse = True,
-                 path = resource_path(context))
-    
-    total_count = api.search_catalog(**query)[0]
-    
-    tag = request.GET.get('tag', None)
-    if tag:
-        query['tags'] = tag
-        
-    # build query string and remove tag=
-    clear_tag_query = request.GET.copy()
-    if 'tag' in clear_tag_query:
-        del clear_tag_query['tag']
-        
-    count, docids = api.search_catalog(**query)
-    get_metadata = api.root.catalog.document_map.get_metadata
-    results = []
-    for docid in docids:
-        #Insert the resolved docid first, since we need to reverse order again.
-        results.insert(0, get_metadata(docid))
-
-    response = {}
-    response['get_polls'] = _get_polls
-    response['polls'] = api.get_restricted_content(context, iface=IPoll, sort_on='created')
-    for poll in response['polls']:
+    polls = api.get_restricted_content(context, iface=IPoll, sort_on='created')
+    for poll in polls:
         try:
             plugin = poll.get_poll_plugin()
         except ComponentLookupError:
@@ -86,12 +42,36 @@ def proposal_listing(context, request, va, **kw):
                         mapping = {'name': poll.get_field_value('poll_plugin')})
             api.flash_messages.add(err_msg, type="error")
             continue
+    
+    query = Eq('path', resource_path(context)) & \
+            Eq('content_type', 'Proposal') & \
+            Any('workflow_state', ('published', 'retracted', 'unhandled'))
+    
+    total_count = api.root.catalog.query(query)[0]
+    
+    tag = request.GET.get('tag', None)
+    if tag:
+        query = query & Any('tags', (tag, ))
+        
+    # build query string and remove tag=
+    clear_tag_query = request.GET.copy()
+    if 'tag' in clear_tag_query:
+        del clear_tag_query['tag']
+        
+    count, docids = api.root.catalog.query(query, sort_index='created', reverse=True)
+    get_metadata = api.root.catalog.document_map.get_metadata
+    results = []
+    for docid in docids:
+        #Insert the resolved docid first, since we need to reverse order again.
+        results.insert(0, get_metadata(docid))
+
+    response = {}
+    response['get_polls'] = _get_polls
+    response['polls'] = polls
     response['clear_tag_url'] = api.request.resource_url(context, query=clear_tag_query)
     response['proposals'] = tuple(results)
     response['hidden_count'] = total_count - count
-    response['api'] = api
-    response['show_retract'] = _show_retract
-    response['translated_state_title'] = _translated_state_title 
+    response['api'] = api 
     return render('../templates/proposals.pt', response, request = request)
 
 @view_action('proposal', 'block')
