3c1e6890c4eb434d2dbc4025f145f1485145052b
Fredrik Nilson <fredrik@betahaus.net>
2012-09-07 16:56:14 +0200

    404 and 403 gives the normal error code when request is xhr and 500 gives a nice looking page with a regular request close #141

--- voteit/core/views/exceptions_view.py
+++ voteit/core/views/exceptions_view.py
@@ -3,6 +3,7 @@ from pyramid.url import resource_url
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.httpexceptions import HTTPFound
 from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPServerError
 from pyramid.security import NO_PERMISSION_REQUIRED
 
 from voteit.core import VoteITMF as _
@@ -23,9 +24,14 @@ class ExceptionView(object):
     
     @view_config(context=HTTPForbidden, permission=NO_PERMISSION_REQUIRED)
     def forbidden_view(self):
-        """ I.e. 403. Find first context where user has view permission
+        """ I.e. 403. If it is a xhr request return Forbidden else 
+            find first context where user has view permission
             if they're logged in, otherwise redirect to login form.
         """
+
+        if self.request.is_xhr:
+            return HTTPForbidden()
+        
         self.api.flash_messages.add(self.exception.detail,
                                     type = 'error',
                                     close_button = False)
@@ -43,11 +49,16 @@ class ExceptionView(object):
 
     @view_config(context=HTTPNotFound, permission=NO_PERMISSION_REQUIRED)
     def not_found_view(self):
-        """ I.e. 404.
+        """ I.e. 404. If it is a xhr request return NotFound else
+            find first context where user has view permission 
         """
         err_msg = _(u"404_error_msg",
                     default = u"Can't find anything at '${path}'. Maybe it has been removed?",
                     mapping = {'path': self.exception.detail})
+        
+        if self.request.is_xhr:
+            return HTTPNotFound(err_msg)
+        
         self.api.flash_messages.add(err_msg,
                                     type = 'error',
                                     close_button = False)
@@ -58,3 +69,20 @@ class ExceptionView(object):
                 return HTTPFound(location = url)
             obj = obj.__parent__
         return HTTPFound(location = self.request.application_url)
+
+
+    @view_config(context=HTTPServerError, renderer="templates/httpexception.pt", permission=NO_PERMISSION_REQUIRED)
+    def server_error_view(self):
+        """ I.e. 500.
+        """
+        err_msg = _(u"500_error_msg",
+                    default = u"The server has either erred or is incapable of performing the requested operation.",)
+        
+        if self.request.is_xhr:
+            return HTTPServerError(err_msg)
+        
+        self.api.flash_messages.add(err_msg,
+                                    type = 'error',
+                                    close_button = False)
+        
+        return {'api': self.api}
