0a4818917e2950394cedff27fe347b3abdcbb5a8
robinharms <robin@betahaus.net>
2012-09-10 10:43:49 +0200

    The schema should handle default code, instead of hacking in things in the view.

--- voteit/core/schemas/discussion_post.py
+++ voteit/core/schemas/discussion_post.py
@@ -6,13 +6,23 @@ from voteit.core import VoteITMF as _
 from voteit.core.validators import deferred_at_enabled_text
 
 
+@colander.deferred
+def deferred_default_discussion_text(node, kw):
+    """ If this is a reply to something else, the default value will be the hashtag
+        of that message.
+    """
+    tag = kw.get('tag', u'')
+    return tag and u"#%s" % tag or u""
+
+
 @schema_factory('DiscussionPostSchema')
 class DiscussionPostSchema(colander.Schema):
     text = colander.SchemaNode(colander.String(),
                                title = _(u"Text"),
-                               validator=deferred_at_enabled_text,
-                               widget=deform.widget.TextAreaWidget(rows=3, cols=40),)
+                               default = deferred_default_discussion_text,
+                               validator = deferred_at_enabled_text,
+                               widget = deform.widget.TextAreaWidget(rows=3, cols=40),)
     tags = colander.SchemaNode(colander.String(),
                                title = _(u"Tags"),
-                               widget=deform.widget.HiddenWidget(),
-                               missing=u'')
+                               widget = deform.widget.HiddenWidget(),
+                               missing = u'')
