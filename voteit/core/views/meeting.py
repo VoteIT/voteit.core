d3c223b3f1396e9bef661816310082b9224245ff
robinharms <robin@betahaus.net>
2012-11-13 14:49:11 +0100

    Split manage agenda things to separate view

--- voteit/core/views/meeting.py
+++ voteit/core/views/meeting.py
@@ -9,23 +9,18 @@ from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid.httpexceptions import HTTPRedirection
-from pyramid.traversal import resource_path
-from pyramid.traversal import find_resource
 from pyramid.renderers import render
 from pyramid.response import Response
 from pyramid.url import resource_url
 from betahaus.pyracont.factories import createContent
 from betahaus.pyracont.factories import createSchema
 from betahaus.viewcomponent.interfaces import IViewGroup
-from repoze.workflow.workflow import WorkflowError
-
 
 from voteit.core import security
 from voteit.core import VoteITMF as _
 from voteit.core.views.base_view import BaseView
 from voteit.core.models.interfaces import IPoll
 from voteit.core.models.interfaces import IMeeting
-from voteit.core.models.agenda_item import AgendaItem
 from voteit.core.models.schemas import add_csrf_token
 from voteit.core.models.schemas import button_save
 from voteit.core.models.schemas import button_add
@@ -363,80 +358,6 @@ class MeetingView(BaseView):
         appstruct = self.context.get_field_appstruct(schema)
         self.response['form'] = form.render(appstruct)
         return self.response
-    
-    @view_config(context=IMeeting, name="handle_agenda_items", renderer="templates/handle_agenda_items.pt", permission=security.EDIT)
-    def handle_agenda_items(self):
-        post = self.request.POST
-        if 'cancel' in self.request.POST:
-            url = self.request.resource_url(self.context, 'handle_agenda_items')
-            return HTTPFound(location = url)
-
-        if 'change' in post:
-            state_id = self.request.POST['state_id']
-            controls = self.request.POST.items()
-            for (k, v) in controls:
-                if k == 'ais':
-                    ai = self.context[v]
-                    try:
-                        ai.set_workflow_state(self.request, state_id)
-                    except WorkflowError, e:
-                        self.api.flash_messages.add(_('Unable to change state on ${title}: ${error}',
-                                                      mapping={'title': ai.title, 'error': e}),
-                                                    type='error')
-            self.api.flash_messages.add(_('States updated'))
-
-        state_info = _dummy_agenda_item.workflow.state_info(None, self.request)
-
-        def _translated_state_title(state):
-            for info in state_info:
-                if info['name'] == state:
-                    return self.api.tstring(info['title'])
-            return state
-        
-        self.response['translated_state_title'] = _translated_state_title
-        self.response['find_resource'] = find_resource
-        self.response['states'] = states = ('ongoing', 'upcoming', 'closed', 'private') 
-        self.response['ais'] = {}
-        context_path = resource_path(self.context)
-        for state in states:
-            query = dict(
-                path = context_path,
-                content_type = 'AgendaItem',
-                sort_index = 'order',
-                workflow_state = state,
-            )
-            self.response['ais'][state] = self.api.get_metadata_for_query(**query)
-
-        return self.response
-    
-    @view_config(context=IMeeting, name="order_agenda_items", renderer="templates/order_agenda_items.pt", permission=security.EDIT)
-    def order_agenda_items(self):
-        self.response['title'] = _(u"order_agenda_items_view_title",
-                                   default = u"Drag and drop agenda items to reorder")
-        post = self.request.POST
-        if 'cancel' in self.request.POST:
-            url = resource_url(self.context, self.request)
-            return HTTPFound(location = url)
-
-        if 'save' in post:
-            controls = self.request.POST.items()
-            ais = []
-            order = 0
-            for (k, v) in controls:
-                if k == 'agenda_items':
-                    ai = self.context[v]
-                    ai.set_field_appstruct({'order': order})
-                    order += 1
-            self.api.flash_messages.add(_('Order updated'))
-            
-        context_path = resource_path(self.context)
-        query = dict(
-            path = context_path,
-            content_type = 'AgendaItem',
-            sort_index = 'order',
-        )
-        self.response['brains'] = self.api.get_metadata_for_query(**query)
-        return self.response
 
     @view_config(context = IMeeting, name = "minutes", renderer = "templates/minutes.pt", permission = security.VIEW)
     def minutes(self):
@@ -459,5 +380,3 @@ class MeetingView(BaseView):
 
         self.response['agenda_items'] = agenda_items
         return self.response
-
-_dummy_agenda_item = AgendaItem()
--- voteit/core/views/tests/test_meeting.py
+++ voteit/core/views/tests/test_meeting.py
@@ -566,44 +566,7 @@ class MeetingViewTests(unittest.TestCase):
         response = obj.form(schema)
         self.assertEqual(response.status, '200 OK')
         self.assertIn("<legend>Presentation</legend>", response.text)
-        
-    def test_order_agenda_items(self):
-        self.config.testing_securitypolicy(userid='dummy',
-                                           permissive=True)
-        context = self._fixture()
-        request = testing.DummyRequest()
-        obj = self._cut(context, request)
-        response = obj.order_agenda_items()
-        self.assertIn('title', response)
-        
-    def test_order_agenda_items_save(self):
-        self.config.scan('voteit.core.subscribers.agenda_item')
-        self.config.include('voteit.core.models.flash_messages')
-        self.config.testing_securitypolicy(userid='dummy',
-                                           permissive=True)
-        context = self._fixture()
-        from voteit.core.models.agenda_item import AgendaItem
-        context['a1'] = AgendaItem(title = 'Agenda Item 1')
-        context['a2'] = AgendaItem(title = 'Agenda Item 2')
-        context['a3'] = AgendaItem(title = 'Agenda Item 3')
-        request = testing.DummyRequest(post = {'save': 'save', 
-                                               'agenda_items': 'a1', 
-                                               'agenda_items': 'a2', 
-                                               'agenda_items': 'a3'},)
-        obj = self._cut(context, request)
-        response = obj.order_agenda_items()
-        self.assertIn('title', response)
-        
-    def test_order_agenda_items_cancel(self):
-        self.config.testing_securitypolicy(userid='dummy',
-                                           permissive=True)
-        self.config.include('voteit.core.models.flash_messages')
-        context = self._fixture()
-        request = testing.DummyRequest(post = {'cancel': 'cancel',})
-        obj = self._cut(context, request)
-        response = obj.order_agenda_items()
-        self.assertEqual(response.location, 'http://example.com/m/')
-        
+
     def test_minutes(self):
         register_catalog(self.config)
         self.config.scan('voteit.core.subscribers.agenda_item')
