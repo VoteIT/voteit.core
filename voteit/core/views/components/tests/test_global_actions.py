366d004b54c6aaddec0898cad5c3f348f1d8054c
robinharms <robin@betahaus.net>
2012-12-14 11:58:09 +0100

    Test fixes due to recent changes

--- voteit/core/views/components/tests/test_global_actions.py
+++ voteit/core/views/components/tests/test_global_actions.py
@@ -33,11 +33,9 @@ class GlobalActionsComponentTests(unittest.TestCase):
                 self.kwargs = kwargs
         return ViewAction(name, title, kwargs)
         
-    def test_global_actions(self):
-        self.config.registry.settings['default_timezone_name'] = "Europe/Stockholm"
-        self.config.registry.settings['default_locale_name'] = 'sv'
-        self.config.include('voteit.core.models.date_time_util')
+    def test_global_actions_authenticated(self):
         self.config.scan('voteit.core.views.components.global_actions')
+        self.config.testing_securitypolicy(userid='admin', permissive=True)
         context = self._fixture()
         request = testing.DummyRequest()
         va = self._va()
@@ -45,37 +43,17 @@ class GlobalActionsComponentTests(unittest.TestCase):
         from voteit.core.views.components.global_actions import global_actions
         response = global_actions(context, request, va, api=api)
         self.assertTrue(response)
-        
-    def test_action_login(self):
+
+    def test_global_actions_anon(self):
+        self.config.scan('voteit.core.views.components.global_actions')
         context = self._fixture()
         request = testing.DummyRequest()
         va = self._va()
         api = self._api(context, request)
-        from voteit.core.views.components.global_actions import action_login
-        response = action_login(context, request, va, api=api)
-        self.assertEqual('<li><a href="http://example.com/login">Login</a></li>', response)
-        
-    def test_action_register(self):
-        context = self._fixture()
-        request = testing.DummyRequest()
-        va = self._va()
-        api = self._api(context, request)
-        from voteit.core.views.components.global_actions import action_register
-        response = action_register(context, request, va, api=api)
-        self.assertEqual('<li><a href="http://example.com/register">Register</a></li>', response)
-        
-    def test_user_profile_action(self):
-        self.config.testing_securitypolicy(userid='admin',
-                                           permissive=True)
-        
-        context = self._fixture()
-        request = testing.DummyRequest()
-        va = self._va()
-        api = self._api(context, request)
-        from voteit.core.views.components.global_actions import user_profile_action
-        response = user_profile_action(context, request, va, api=api)
-        self.assertEqual('<li><a href="http://example.com/users/admin/" class="user icon"><span>VoteIT Administrator</span></a></li>', response)
-        
+        from voteit.core.views.components.global_actions import global_actions
+        response = global_actions(context, request, va, api=api)
+        self.assertFalse(response)
+
     def test_logout_action(self):
         context = self._fixture()
         request = testing.DummyRequest()
