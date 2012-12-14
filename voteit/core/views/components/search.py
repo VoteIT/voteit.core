9a483a5309e2793308482a7e68d5a87729ab61ba
robinharms <robin@betahaus.net>
2012-12-14 14:28:11 +0100

    Search links to actual form

--- voteit/core/views/components/search.py
+++ voteit/core/views/components/search.py
@@ -6,8 +6,12 @@ from voteit.core.security import VIEW
 from voteit.core.models.interfaces import IMeeting
 
 
-@view_action('main', 'search', permission=VIEW)
+@view_action('main', 'search', permission=VIEW, containment = IMeeting)
 def meeting_actions(context, request, va, **kw):
     api = kw['api']
     response = {'api': api}
+    #FIXME: discriminators don't work on render single view component. Untill that has
+    #been fixed or it's actually possible to search in root, keep this code:
+    if not api.meeting:
+        return u""
     return render('templates/header/search.pt', response, request = request)
