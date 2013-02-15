4852a9f79c2f14b43168c1a80588b57d2bd7818f
robinharms <robin@betahaus.net>
2013-02-15 10:17:43 +0100

    Cogwheel menu duplicates was due to checking parents and triggering on the wrong selector. Fixes #245

--- voteit/core/js/voteit_moderator.js
+++ voteit/core/js/voteit_moderator.js
@@ -1,22 +1,10 @@
 
 /* Cogwheel menus */
-$('.cogwheel').live('hover', function(event) {
+$('.cogwheel a').live('hover', function(event) {
     /* stop form using default action 
     IE might throw an error calling preventDefault(), so use a try/catch block. */
     try { event.preventDefault(); } catch(e) {}
-    if ($(this).hasAttr('url')) {
-        var url = $(this).attr('url');
-        var q_content = { 
-            text: voteit.translation['loading'], // The text to use whilst the AJAX request is loading
-            ajax: {
-                url: url,
-            }
-        };
-    }
-    else {
-        //We need a copy of the content, otherwise qtip will try to locate it every time it triggers
-        var q_content = { text: $(this).parent().find('.menu_body').clone() };
-    }
+    var q_content = { text: $(this).parent().find('.menu_body').clone() };
     $(this).qtip({
         overwrite: false, // Make sure the tooltip won't be overridden once created
         content: q_content,
