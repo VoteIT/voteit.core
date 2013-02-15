1c7bf871e5c9f55f15d6b3b0f1043eceb5f01cc5
robinharms <robin@betahaus.net>
2013-02-15 17:19:03 +0100

    Showing a more sane title in title slot

--- voteit/core/views/components/head.py
+++ voteit/core/views/components/head.py
@@ -3,5 +3,15 @@ from betahaus.viewcomponent import view_action
 
 @view_action('head', 'title')
 def page_title(context, request, va, **kw):
-    #FIXME: Proper title based on page name etc? Remember that returing a string won't render translations
-    return """<title>VoteIT</title>"""
+    api = kw['api']
+    if api.meeting:
+        if context == api.meeting:
+            title = api.meeting.title
+        else:
+            title = u"%s | %s" % (context.title, api.meeting.title)
+    else:
+        if context == api.root:
+            title = api.root.title
+        else:
+            title = u"%s | %s" % (context.title, api.root.title)
+    return u"""<title>%s</title>""" % title
