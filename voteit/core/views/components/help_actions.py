8729c0682ef2fd91ea40cc1acebf5787e426ec4e
robinharms <robin@betahaus.net>
2013-02-18 17:46:10 +0100

    Request new password link in help menu too

--- voteit/core/views/components/help_actions.py
+++ voteit/core/views/components/help_actions.py
@@ -1,19 +1,25 @@
-from pyramid.renderers import render
 from betahaus.viewcomponent import view_action
 
 from voteit.core import VoteITMF as _
 
 
 @view_action('help_action', 'wiki')
-def action_wiki(context, request, va, **kw):
+def action_manual(context, request, va, **kw):
     api = kw['api']
-    return """<li><a href="http://wiki.voteit.se" target="_blank">%s</a></li>""" % (api.translate(_(u"VoteIT Manual")),)
-
+    return u"""<li><a href="http://wiki.voteit.se" target="_blank">%s</a></li>""" % (api.translate(_(u"VoteIT Manual")),)
 
 @view_action('help_action', 'contact')
 def action_contact(context, request, va, **kw):
     api = kw['api']
     if api.meeting:
-        return """<li><a href="%s">%s</a></li>""" % (request.resource_url(api.meeting, 'contact'),
+        return u"""<li><a href="%s">%s</a></li>""" % (request.resource_url(api.meeting, 'contact'),
                                                                            api.translate(_(u"Contact")),)
-    return ""
+    return u""
+
+@view_action('help_action', 'lost_password')
+def action_lost_password(context, request, va, **kw):
+    api = kw['api']
+    if api.userid:
+        return u""
+    return u"""<li><a href="%s">%s</a></li>""" % (request.resource_url(api.root, 'request_password'),
+                                                                       api.translate(_(u"Recover password")),)
