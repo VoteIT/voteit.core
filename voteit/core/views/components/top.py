a99290af499614913d5ae151e1e9a37c3551a6c6
robinharms <robin@betahaus.net>
2013-02-19 11:01:58 +0100

    Overflow on some windows containing descriptions. Closes #250

--- voteit/core/views/components/top.py
+++ voteit/core/views/components/top.py
@@ -8,4 +8,4 @@ from voteit.core import VoteITMF as _
 def display_context_description(context, request, *args, **kwargs):
     """ Can be used in lots of places to simply get the context description. This is to make it orderable.
     """
-    return """<div class="description">%s</div>""" % context.description
+    return """<div class="description">%s<div class="clear"><!-- --></div></div>""" % context.description
