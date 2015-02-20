7ac479604b5af231d06a75d67dddf31bd4b1b12b
robinharms <robin@betahaus.net>
2015-02-20 14:04:44 +0100

    Loading agenda items menu with cogwheels

--- voteit/core/views/portlets/agenda.py
+++ voteit/core/views/portlets/agenda.py
@@ -6,10 +6,11 @@ from pyramid.traversal import find_interface
 from pyramid.traversal import find_root
 from pyramid.traversal import resource_path
 import colander
+from arche.views.base import BaseView
 
 from voteit.core.models.interfaces import IMeeting
-from voteit.core.fanstaticlib import ai_portlet_js
 from voteit.core import _
+from voteit.core.fanstaticlib import data_loader
 
 class AgendaPortlet(PortletType):
     name = "agenda"
@@ -19,36 +20,58 @@ class AgendaPortlet(PortletType):
     def render(self, context, request, view, **kwargs):
         meeting = find_interface(context, IMeeting)
         if meeting:
-            ai_portlet_js.need()
+            data_loader.need()
             response = {'title': self.title,
                         'portlet': self.portlet,
-                        'meeting': meeting,
                         'view': view,
-                        'data_url': request.resource_url(meeting, '__agenda_items__.json')}
+                        'load_url': request.resource_url(meeting, '__agenda_items__')}
             return render("voteit.core:views/templates/portlets/agenda.pt",
                           response,
                           request = request)
 
 
+class AgendaInlineView(BaseView):
 
-states = ('ongoing', 'upcoming', 'closed', 'private')
+    def __call__(self):
+        response = {}
+        response['states'] = ('ongoing', 'upcoming', 'closed', 'private')
+        response['meeting'] = self.request.meeting
+        return response
 
-def jsondata_agenda(context, request):
-    root = find_root(context)
-    path = resource_path(context)
-    results = []
-    for state in states:
-        state_res = []
-        for docid in root.catalog.search(path = path, type_name = 'AgendaItem', workflow_state = state, sort_index = 'order')[1]:
+    def get_ais(self, state):
+        path = resource_path(self.context)
+        results = []
+        catalog = self.request.root.catalog
+        for docid in catalog.search(path = path,
+                                    type_name = 'AgendaItem',
+                                    workflow_state = state,
+                                    sort_index = 'order')[1]:
             try:
-                meta = dict(root.document_map.get_metadata(docid))
+                meta = dict(self.request.root.document_map.get_metadata(docid))
             except KeyError:
                 meta = {}
-            state_res.append( meta )
-        results.append({'state': state, 'state_ais': state_res}) 
-    return results
+            results.append(meta)
+        return results
+
+# def jsondata_agenda(context, request):
+#     root = find_root(context)
+#     path = resource_path(context)
+#     results = []
+#     for state in states:
+#         state_res = []
+#         for docid in root.catalog.search(path = path, type_name = 'AgendaItem', workflow_state = state, sort_index = 'order')[1]:
+#             try:
+#                 meta = dict(root.document_map.get_metadata(docid))
+#             except KeyError:
+#                 meta = {}
+#             state_res.append( meta )
+#         results.append({'state': state, 'state_ais': state_res}) 
+#     return results
 
 
 def includeme(config):
-    config.add_view(jsondata_agenda, name = '__agenda_items__.json', context = IMeeting, renderer = 'json')
+    config.add_view(AgendaInlineView,
+                    name = '__agenda_items__',
+                    context = IMeeting,
+                    renderer = 'voteit.core:views/templates/portlets/agenda_inline.pt')
     config.add_portlet(AgendaPortlet)
