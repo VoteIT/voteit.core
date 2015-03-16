5602857159f654e8dd3b0656f57e28f0bd69c322
robinharms <robin@betahaus.net>
2015-03-16 16:32:06 +0100

    Not needed with arche

--- voteit/core/views/components/discussions.py
+++ voteit/core/views/components/discussions.py
@@ -1,113 +1,113 @@
-# -*- coding:utf-8 -*-
-
-from betahaus.viewcomponent import view_action
-from pyramid.traversal import resource_path
-from pyramid.traversal import find_resource
-from pyramid.traversal import find_interface
-from pyramid.renderers import render
-
-from voteit.core import VoteITMF as _
-from voteit.core.models.interfaces import IMeeting
-from voteit.core.security import DELETE
-from voteit.core.htmltruncate import htmltruncate
-
-import logging
-log = logging.getLogger(__name__)
-
-#FIXME: needs a way to set default value on this on creation of meeting
-#FIXME: This should not be here - MOVE!
-def truncate(text, length=240):
-    try:
-        if length and length > 0:
-            trunc_text = htmltruncate.truncate(text, length, u'…')
-        else:
-            trunc_text = text
-    # If the html tags doesn't match up return the complete text
-    except htmltruncate.UnbalancedError: # pragma : no cover
-        trunc_text = text
-    except Exception, e:
-        trunc_text = text
-        log.error("Could not truncate text: %s", text)
-
-    return (trunc_text, text != trunc_text)
-
-@view_action('discussions', 'listing')
-def discussions_listing(context, request, va, **kw):
-    """ Get discussions for a specific context """
-    api = kw['api']
-
-    def _show_delete(brain):
-        #Do more expensive checks last!
-        if not api.userid in brain['creators']:
-            return
-        obj = find_resource(api.root, brain['path'])
-        return api.context_has_permission(DELETE, obj)
-
-    path = resource_path(context)
-    
-    tag = request.GET.get('tag', None)
-    
-    query = dict(path = path,
-                 content_type='DiscussionPost')
-    
-    query['sort_index'] = 'created'
-    query['reverse'] = True
-    
-    total_count = api.search_catalog(**query)[0]
-    
-    if tag:
-        query['tags'] = tag
-
-    if request.GET.get('discussions', '') == 'all' or tag:
-        limit = 0
-    else:
-        unread_count = api.search_catalog(unread = api.userid, **query)[0]
-        limit = 5
-        if unread_count > limit:
-            limit = unread_count
-
-    
-    #Returns tuple of (item count, iterator with docids)
-    count = api.search_catalog(**query)[0]
-
-    #New query with only limited number of results
-    if limit:
-        query['limit'] = limit
-    docids = api.search_catalog(**query)[1]
-    get_metadata = api.root.catalog.document_map.get_metadata
-    results = []
-    for docid in docids:
-        #Insert the resolved docid first, since we need to reverse order again.
-        results.insert(0, get_metadata(docid))
-        
-    #Get truncate length from meeting
-    meeting = find_interface(context, IMeeting)
-    #FIXME: needs a way to set default value on this on creation of meeting
-    truncate_length = meeting.get_field_value('truncate_discussion_length', 240)
-    
-    # build query string and remove discussions=all
-    more_query = request.GET.copy()
-    if 'discussions' in more_query: 
-        del more_query['discussions']
-    
-    # build query string and remove tag=
-    clear_tag_query = request.GET.copy()
-    if 'tag' in clear_tag_query:
-        del clear_tag_query['tag'] 
-        
-    response = {}
-    response['clear_tag_url'] = api.request.resource_url(context, query=clear_tag_query)
-    response['more_url_all'] = api.request.resource_url(context, 'discussions', query=dict(more_query.items()+{'discussions': 'all'}.items()))
-    response['more_url_normal'] = api.request.resource_url(context, 'discussions', query=more_query)
-    response['discussions'] = tuple(results)
-    if limit and limit < count:
-        response['over_limit'] = count - limit
-    else:
-        response['over_limit'] = 0
-    response['hidden_count'] = total_count - count
-    response['limit'] = limit
-    response['api'] = api
-    response['show_delete'] = _show_delete
-    response['truncate'] = truncate 
-    response['truncate_length'] = truncate_length
-    return render('../templates/discussions.pt', response, request = request)
+# # -*- coding:utf-8 -*-
+# 
+# from betahaus.viewcomponent import view_action
+# from pyramid.traversal import resource_path
+# from pyramid.traversal import find_resource
+# from pyramid.traversal import find_interface
+# from pyramid.renderers import render
+# 
+# from voteit.core import VoteITMF as _
+# from voteit.core.models.interfaces import IMeeting
+# from voteit.core.security import DELETE
+# from voteit.core.htmltruncate import htmltruncate
+# 
+# import logging
+# log = logging.getLogger(__name__)
+# 
+# #FIXME: needs a way to set default value on this on creation of meeting
+# #FIXME: This should not be here - MOVE!
+# def truncate(text, length=240):
+#     try:
+#         if length and length > 0:
+#             trunc_text = htmltruncate.truncate(text, length, u'…')
+#         else:
+#             trunc_text = text
+#     # If the html tags doesn't match up return the complete text
+#     except htmltruncate.UnbalancedError: # pragma : no cover
+#         trunc_text = text
+#     except Exception, e:
+#         trunc_text = text
+#         log.error("Could not truncate text: %s", text)
+# 
+#     return (trunc_text, text != trunc_text)
+# 
+# @view_action('discussions', 'listing')
+# def discussions_listing(context, request, va, **kw):
+#     """ Get discussions for a specific context """
+#     api = kw['api']
+# 
+#     def _show_delete(brain):
+#         #Do more expensive checks last!
+#         if not api.userid in brain['creators']:
+#             return
+#         obj = find_resource(api.root, brain['path'])
+#         return api.context_has_permission(DELETE, obj)
+# 
+#     path = resource_path(context)
+#     
+#     tag = request.GET.get('tag', None)
+#     
+#     query = dict(path = path,
+#                  content_type='DiscussionPost')
+#     
+#     query['sort_index'] = 'created'
+#     query['reverse'] = True
+#     
+#     total_count = api.search_catalog(**query)[0]
+#     
+#     if tag:
+#         query['tags'] = tag
+# 
+#     if request.GET.get('discussions', '') == 'all' or tag:
+#         limit = 0
+#     else:
+#         unread_count = api.search_catalog(unread = api.userid, **query)[0]
+#         limit = 5
+#         if unread_count > limit:
+#             limit = unread_count
+# 
+#     
+#     #Returns tuple of (item count, iterator with docids)
+#     count = api.search_catalog(**query)[0]
+# 
+#     #New query with only limited number of results
+#     if limit:
+#         query['limit'] = limit
+#     docids = api.search_catalog(**query)[1]
+#     get_metadata = api.root.catalog.document_map.get_metadata
+#     results = []
+#     for docid in docids:
+#         #Insert the resolved docid first, since we need to reverse order again.
+#         results.insert(0, get_metadata(docid))
+#         
+#     #Get truncate length from meeting
+#     meeting = find_interface(context, IMeeting)
+#     #FIXME: needs a way to set default value on this on creation of meeting
+#     truncate_length = meeting.get_field_value('truncate_discussion_length', 240)
+#     
+#     # build query string and remove discussions=all
+#     more_query = request.GET.copy()
+#     if 'discussions' in more_query: 
+#         del more_query['discussions']
+#     
+#     # build query string and remove tag=
+#     clear_tag_query = request.GET.copy()
+#     if 'tag' in clear_tag_query:
+#         del clear_tag_query['tag'] 
+#         
+#     response = {}
+#     response['clear_tag_url'] = api.request.resource_url(context, query=clear_tag_query)
+#     response['more_url_all'] = api.request.resource_url(context, 'discussions', query=dict(more_query.items()+{'discussions': 'all'}.items()))
+#     response['more_url_normal'] = api.request.resource_url(context, 'discussions', query=more_query)
+#     response['discussions'] = tuple(results)
+#     if limit and limit < count:
+#         response['over_limit'] = count - limit
+#     else:
+#         response['over_limit'] = 0
+#     response['hidden_count'] = total_count - count
+#     response['limit'] = limit
+#     response['api'] = api
+#     response['show_delete'] = _show_delete
+#     response['truncate'] = truncate 
+#     response['truncate_length'] = truncate_length
+#     return render('../templates/discussions.pt', response, request = request)
--- voteit/core/views/components/proposals.py
+++ voteit/core/views/components/proposals.py
@@ -1,99 +1,99 @@
-from betahaus.viewcomponent import view_action
-from pyramid.renderers import render
-from pyramid.traversal import resource_path
-from repoze.catalog.query import Any
-from repoze.catalog.query import Eq
-from repoze.catalog.query import NotAny
-from zope.component.interfaces import ComponentLookupError
-
-from voteit.core import VoteITMF as _
-from voteit.core.models.interfaces import IPoll
-from voteit.core.models.interfaces import IAgendaItem
-
-
-def proposal_response(context, request, va, **kw):
-    """ Prepair a response dict for the proposal view.
-    """
-    api = kw['api']
-    #Start with checking which polls that exist in this context and store shown uids
-    shown_uids = set()
-    polls = []
-    for poll in api.get_restricted_content(context, iface=IPoll, sort_on='created'):
-        try:
-            plugin = poll.get_poll_plugin()
-        except ComponentLookupError:
-            err_msg = _(u"plugin_missing_error",
-                        default = u"Can't find any poll plugin with name '${name}'. Perhaps that package has been uninstalled?",
-                        mapping = {'name': poll.get_field_value('poll_plugin')})
-            api.flash_messages.add(err_msg, type="error")
-            continue
-        shown_uids.update(poll.proposal_uids)
-        polls.append(poll)
-
-    #The agenda item query must be based on the context and to exclude all already shown
-    #This view also cares about retracted proposals where as poll version doesn't
-    query = Eq('path', resource_path(context)) &\
-            Eq('content_type', 'Proposal') &\
-            NotAny('uid', shown_uids)
-
-    hide_retracted = api.meeting.get_field_value('hide_retracted', False)
-    hide_unhandled = api.meeting.get_field_value('hide_unhandled_proposals', False)
-    hidden_states = []
-    if hide_retracted:
-        hidden_states.append('retracted')
-    if hide_unhandled:
-        hidden_states.append('unhandled')
-    if hidden_states:
-        query &= NotAny('workflow_state', hidden_states)
-
-    tag = request.GET.get('tag', None)
-    if tag:
-        #Only apply tag limit for things that aren't polls.
-        #This is a safegard against user errors
-        query &= Any('tags', (tag, ))
-        
-    # build query string and remove tag
-    clear_tag_query = request.GET.copy()
-    if 'tag' in clear_tag_query:
-        del clear_tag_query['tag']
-
-    count, docids = api.root.catalog.query(query, sort_index='created')
-    get_metadata = api.root.catalog.document_map.get_metadata
-    results = [get_metadata(x) for x in docids]
-    hidden_proposals = []
-    if hidden_states:
-        query = Eq('path', resource_path(context)) &\
-                Eq('content_type', 'Proposal') &\
-                NotAny('uid', shown_uids) &\
-                Any('workflow_state', hidden_states)
-        if tag:
-            query &= Any('tags', (tag, ))
-        count, docids = api.root.catalog.query(query, sort_index='created')
-        hidden_proposals = [get_metadata(x) for x in docids]
-
-    response = {}
-    response['clear_tag_url'] = request.resource_url(context, query=clear_tag_query)
-    response['proposals'] = tuple(results)
-    response['hidden_proposals'] = tuple(hidden_proposals)
-    response['tag'] = tag
-    response['api'] = api 
-    response['polls'] = polls
-    return response
-
-@view_action('proposals', 'listing', interface = IAgendaItem)
-def proposal_listing(context, request, va, **kw):
-    """ Get proposals for a specific context.
-    """
-    return render('templates/proposals/listing.pt',
-                  proposal_response(context, request, va, **kw),
-                  request = request)
-
-@view_action('proposal', 'block')
-def proposal_block(context, request, va, **kw):
-    """ This is the view group for each individual proposal. """
-    api = kw['api']
-    brain = kw['brain']
-    response = {}
-    response['api'] = api
-    response['brain'] = brain
-    return render('templates/proposals/proposal.pt', response, request=request)
+# from betahaus.viewcomponent import view_action
+# from pyramid.renderers import render
+# from pyramid.traversal import resource_path
+# from repoze.catalog.query import Any
+# from repoze.catalog.query import Eq
+# from repoze.catalog.query import NotAny
+# from zope.component.interfaces import ComponentLookupError
+# 
+# from voteit.core import VoteITMF as _
+# from voteit.core.models.interfaces import IPoll
+# from voteit.core.models.interfaces import IAgendaItem
+# 
+# 
+# def proposal_response(context, request, va, **kw):
+#     """ Prepair a response dict for the proposal view.
+#     """
+#     api = kw['api']
+#     #Start with checking which polls that exist in this context and store shown uids
+#     shown_uids = set()
+#     polls = []
+#     for poll in api.get_restricted_content(context, iface=IPoll, sort_on='created'):
+#         try:
+#             plugin = poll.get_poll_plugin()
+#         except ComponentLookupError:
+#             err_msg = _(u"plugin_missing_error",
+#                         default = u"Can't find any poll plugin with name '${name}'. Perhaps that package has been uninstalled?",
+#                         mapping = {'name': poll.get_field_value('poll_plugin')})
+#             api.flash_messages.add(err_msg, type="error")
+#             continue
+#         shown_uids.update(poll.proposal_uids)
+#         polls.append(poll)
+# 
+#     #The agenda item query must be based on the context and to exclude all already shown
+#     #This view also cares about retracted proposals where as poll version doesn't
+#     query = Eq('path', resource_path(context)) &\
+#             Eq('content_type', 'Proposal') &\
+#             NotAny('uid', shown_uids)
+# 
+#     hide_retracted = api.meeting.get_field_value('hide_retracted', False)
+#     hide_unhandled = api.meeting.get_field_value('hide_unhandled_proposals', False)
+#     hidden_states = []
+#     if hide_retracted:
+#         hidden_states.append('retracted')
+#     if hide_unhandled:
+#         hidden_states.append('unhandled')
+#     if hidden_states:
+#         query &= NotAny('workflow_state', hidden_states)
+# 
+#     tag = request.GET.get('tag', None)
+#     if tag:
+#         #Only apply tag limit for things that aren't polls.
+#         #This is a safegard against user errors
+#         query &= Any('tags', (tag, ))
+#         
+#     # build query string and remove tag
+#     clear_tag_query = request.GET.copy()
+#     if 'tag' in clear_tag_query:
+#         del clear_tag_query['tag']
+# 
+#     count, docids = api.root.catalog.query(query, sort_index='created')
+#     get_metadata = api.root.catalog.document_map.get_metadata
+#     results = [get_metadata(x) for x in docids]
+#     hidden_proposals = []
+#     if hidden_states:
+#         query = Eq('path', resource_path(context)) &\
+#                 Eq('content_type', 'Proposal') &\
+#                 NotAny('uid', shown_uids) &\
+#                 Any('workflow_state', hidden_states)
+#         if tag:
+#             query &= Any('tags', (tag, ))
+#         count, docids = api.root.catalog.query(query, sort_index='created')
+#         hidden_proposals = [get_metadata(x) for x in docids]
+# 
+#     response = {}
+#     response['clear_tag_url'] = request.resource_url(context, query=clear_tag_query)
+#     response['proposals'] = tuple(results)
+#     response['hidden_proposals'] = tuple(hidden_proposals)
+#     response['tag'] = tag
+#     response['api'] = api 
+#     response['polls'] = polls
+#     return response
+# 
+# @view_action('proposals', 'listing', interface = IAgendaItem)
+# def proposal_listing(context, request, va, **kw):
+#     """ Get proposals for a specific context.
+#     """
+#     return render('templates/proposals/listing.pt',
+#                   proposal_response(context, request, va, **kw),
+#                   request = request)
+# 
+# @view_action('proposal', 'block')
+# def proposal_block(context, request, va, **kw):
+#     """ This is the view group for each individual proposal. """
+#     api = kw['api']
+#     brain = kw['brain']
+#     response = {}
+#     response['api'] = api
+#     response['brain'] = brain
+#     return render('templates/proposals/proposal.pt', response, request=request)
