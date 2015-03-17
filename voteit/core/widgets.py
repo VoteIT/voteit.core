f95e68ff5c02a8a42bb71143e1db7d3dab410fd5
robinharms <robin@betahaus.net>
2015-03-17 10:55:54 +0100

    Should move to another package

--- voteit/core/widgets.py
+++ voteit/core/widgets.py
@@ -1,16 +1,16 @@
-from deform.widget import RadioChoiceWidget
-
-#from voteit.core.fanstaticlib import star_rating
-
-
-class StarWidget(RadioChoiceWidget):
-    """ Star widget for raiting alternatives.
-        Use keyword creator_info to pass along creator information.
-        See voteit.schulze.models for example code.
-    """
-    template = 'star_choice'
-    readonly_template = 'readonly/star_choice'
-
-    def __init__(self, **kw):
-        super(StarWidget, self).__init__(**kw)
+# from deform.widget import RadioChoiceWidget
+# 
+# #from voteit.core.fanstaticlib import star_rating
+# 
+# 
+# class StarWidget(RadioChoiceWidget):
+#     """ Star widget for raiting alternatives.
+#         Use keyword creator_info to pass along creator information.
+#         See voteit.schulze.models for example code.
+#     """
+#     template = 'star_choice'
+#     readonly_template = 'readonly/star_choice'
+# 
+#     def __init__(self, **kw):
+#         super(StarWidget, self).__init__(**kw)
         #star_rating.need()
