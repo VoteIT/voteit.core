cffb758b5b8e57aee0c89fede5e6637674f0c2c5
robinharms <robin@betahaus.net>
2015-04-29 10:28:27 +0200

    Work in progress on hiding previous and taking care of that kind of filtering

--- voteit/core/js/agenda_item.js
+++ voteit/core/js/agenda_item.js
@@ -88,6 +88,7 @@ DiscussionHandler.prototype.load_previous = function(event) {
     var that = this;
     request.done(function(response) {
       that.handle_response(response, true);
+      $('[data-ai-method="hide_previous"]').show();
     });
     request.fail(function() {
       elem.show()
@@ -96,16 +97,40 @@ DiscussionHandler.prototype.load_previous = function(event) {
   }
 }
 
+DiscussionHandler.prototype.hide_previous = function(event) {
+  if (typeof event != 'undefined') event.preventDefault();
+  //Loop through items, show last 5 hide the rest
+  var elems = $('[data-type-name="DiscussionPost"]')
+  if (elems.length > 5) {
+    elems.slice(0, elems.length - 5).each(function(i, val) {
+      $(val).hide();
+    });
+  }
+  $('[data-ai-method="hide_previous"]').hide();
+  $('[data-ai-method="show_previous"]').show();
+}
+DiscussionHandler.prototype.show_previous = function(event) {
+  if (typeof event != 'undefined') event.preventDefault();
+  voteit.ai_filter.apply_filter();
+  //$('[data-type-name="DiscussionPost"]').slideDown();
+  $('[data-ai-method="hide_previous"]').show();
+  $('[data-ai-method="show_previous"]').hide();
+}
+
 
 /* FILTERING */
 var FilterHandler = function() {
   this.filter_tags = [];
   this.show_hidden = false;
+  this.current_docids = [];
 }
 
 FilterHandler.prototype.apply_filter = function(show_docids) {
+  if (typeof show_docids != 'undefined') {
+    this.current_docids = show_docids;
+  }
   $('[data-type-name]').hide();
-  $.each(show_docids, function(i, val) {
+  $.each(this.current_docids, function(i, val) {
     $('[data-docid="' + val + '"]').show();
   });
 }
@@ -147,6 +172,11 @@ FilterHandler.prototype.handle_filter_update = function(event) {
   if ($(event.currentTarget).data('tag-reset') == true) {
     this.reset();
   }
+  
+  this.request_update(url);
+  /* FIXME: Should the filter change and update if other things aren't loaded?
+   * Must require tags to be set too
+   * 
   if (typeof voteit.discussion != 'undefined' && voteit.discussion.previous_fetched == false) {
     var request = voteit.discussion.load_previous();
     var that = this;
@@ -158,6 +188,7 @@ FilterHandler.prototype.handle_filter_update = function(event) {
   } else {
     this.request_update(url);
   }
+  */
 }
 
 FilterHandler.prototype.request_update = function(url) {
@@ -169,6 +200,12 @@ FilterHandler.prototype.request_update = function(url) {
 
 $(document).ready(function() {
   voteit.ai_filter = new FilterHandler();
+  $('body').on('click', '[data-ai-method="hide_previous"]', function(event) {
+    voteit.discussion.hide_previous(event);
+  });
+  $('body').on('click', '[data-ai-method="show_previous"]', function(event) {
+    voteit.discussion.show_previous(event);
+  });
   $('body').on('click', '[data-ai-method="load_previous_url"]', function(event) {
     voteit.discussion.load_previous(event);
   });
