311f19c3d092076723506128058e413570952900
Robin Harms Oredsson <robin@betahaus.net>
2017-05-16 20:05:34 +0200

    Cleanup and structure

--- voteit/core/js/data_loader.js
+++ voteit/core/js/data_loader.js
@@ -17,7 +17,8 @@ voteit.load_target = function (target) {
     return request
 }
 
-function reload_target(event) {
+
+voteit.reload_target = function (event) {
   event.preventDefault();
   var elem = $(event.currentTarget);
   arche.actionmarker_feedback(elem, true);
@@ -27,12 +28,11 @@ function reload_target(event) {
     arche.load_flash_messages();
   });
 }
-voteit.reload_target = reload_target;
 
 
 /* Adds an attribute to regular popover events and initializes them.
  * Fetches content for the popover from an external source (href) */
-function external_popover_from_event(event) {
+voteit.external_popover_from_event = function (event) {
   event.preventDefault();
   var elem = $(event.currentTarget);
   if (elem.data('external-popover-loaded') == false) {
@@ -46,7 +46,7 @@ function external_popover_from_event(event) {
     request.fail(arche.flash_error);
   }
 }
-voteit.external_popover_from_event = external_popover_from_event;
+
 
 voteit.load_and_replace = function (event) {
   event.preventDefault();
@@ -70,94 +70,11 @@ voteit.load_and_replace = function (event) {
 }
 
 
-voteit.load_polls_menu = function (event) {
-  // Remember that the menu could be closed via a click too.
-  var elem = $(event.currentTarget);
-  if (elem.data('menu-loaded') == true) return;
-  voteit.reset_polls_menu();
-  var menu_target = $('[data-polls-menu-target]');
-  arche.actionmarker_feedback(menu_target, true);
-  var url = elem.data('polls-menu');
-  var request = arche.do_request(url);
-  request.done(function(response) {
-    menu_target.html(response);
-    elem.data('menu-loaded', true)
-  });
-  request.fail(function(jqxhr) {
-    arche.flash_error(jqxhr);
-    arche.actionmarker_feedback(menu_target, false);
-  });
-}
-
-
-voteit.reset_polls_menu = function() {
-  var out = '<li role="presentation" class="disabled">';
-  out += '<a role="menuitem" tabindex="-1" href="#">';
-  out += '<span data-actionmarker="glyphicon glyphicon-refresh rotate-me"></span>';
-  out += $('[data-polls-menu-target]').data('placeholder');
-  out += '</a></li>';
-  $('[data-menu-loaded]').data('menu-loaded', false);
-  $('[data-polls-menu-target]').html(out);
-}
-
-
-
-voteit.set_poll_proposal = function(event) {
-    var elem = $(event.currentTarget);
-    var form = elem.parents('form');
-    arche.actionmarker_feedback(elem, true);
-    var request = arche.do_request(elem.data('url'), {data: form.serialize(), method: 'POST'});
-    request.done(function(response) {
-        var poll_elem = $('[data-uid="' + elem.data('set-poll') + '"]');
-        poll_elem.replaceWith(response);
-    });
-    request.always(function() {
-        arche.actionmarker_feedback(elem, false);
-    });
-}
-
-
-voteit.pick_poll = function(event) {
-    event.preventDefault();
-    var elem = $(event.currentTarget);
-    var url = elem.attr('href');
-    var will_remove = $(elem.data('will-remove'));
-    console.log(url)
-    if (will_remove.length > 0) {
-        //Remove
-        will_remove.remove();
-    } else {
-        arche.actionmarker_feedback(elem, true);
-        //Load
-        var request = arche.do_request(url);
-        request.done(function(response) {
-            $.each(response, function(i, val) {
-                $('[data-uid="' + i + '"]').after(val);
-            });
-        });
-        request.always(function() {
-            arche.actionmarker_feedback(elem, false);
-        });
-    }
-}
-
-
-voteit.close_pick_poll = function(event) {
-    $("[data-pick-poll-context]").remove();
-}
-
-
 $(document).ready(function () {
   $("[data-load-target]").each(function() {
     voteit.load_target(this);
   });
   $('body').on('click', '[data-reload-target]', voteit.reload_target);
-  $('#polls-menu').on('hidden.bs.dropdown', voteit.reset_polls_menu);
   $('body').on('click', '[data-clickable-target]', voteit.load_and_replace);
-  $('body').on('click', '[data-polls-menu]', voteit.load_polls_menu);
   $('body').on('click', '[data-external-popover-loaded="false"]', voteit.external_popover_from_event);
-  $('body').on('click', '[data-set-poll]', voteit.set_poll_proposal);
-  $('body').on('click', '[data-close-pick-poll]', voteit.close_pick_poll);
-  $('body').on('click', '[data-pick-poll]', voteit.pick_poll);
-
 });
--- voteit/core/js/voteit_moderator.js
+++ voteit/core/js/voteit_moderator.js
@@ -2,7 +2,7 @@ if(typeof(voteit) == "undefined"){
   voteit = {};
 }
 /* attached to the template cogwheel.pt */
-function load_cog_menu(event) {
+voteit.load_cog_menu = function(event) {
   event.preventDefault();
   var elem = $(event.currentTarget);
   arche.actionmarker_feedback(elem, true);
@@ -17,8 +17,57 @@ function load_cog_menu(event) {
     arche.actionmarker_feedback(elem, false);
   })
 }
-voteit.load_cog_menu = load_cog_menu;
+
+
+/* fixme: refactor to use save button instead of this */
+voteit.set_poll_proposal = function(event) {
+    var elem = $(event.currentTarget);
+    var form = elem.parents('form');
+    arche.actionmarker_feedback(elem, true);
+    var request = arche.do_request(elem.data('url'), {data: form.serialize(), method: 'POST'});
+    request.done(function(response) {
+        var poll_elem = $('[data-uid="' + elem.data('set-poll') + '"]');
+        poll_elem.replaceWith(response);
+    });
+    request.always(function() {
+        arche.actionmarker_feedback(elem, false);
+    });
+}
+
+
+voteit.pick_poll = function(event) {
+    event.preventDefault();
+    var elem = $(event.currentTarget);
+    var url = elem.attr('href');
+    var will_remove = $(elem.data('will-remove'));
+    console.log(url)
+    if (will_remove.length > 0) {
+        //Remove
+        will_remove.remove();
+    } else {
+        arche.actionmarker_feedback(elem, true);
+        //Load
+        var request = arche.do_request(url);
+        request.done(function(response) {
+            $.each(response, function(i, val) {
+                $('[data-uid="' + i + '"]').after(val);
+            });
+        });
+        request.always(function() {
+            arche.actionmarker_feedback(elem, false);
+        });
+    }
+}
+
+
+voteit.close_pick_poll = function(event) {
+    $("[data-pick-poll-context]").remove();
+}
+
 
 $(document).ready(function() {
-  $('body').on('click', '[data-cogwheel-menu]', voteit.load_cog_menu);
+    $('body').on('click', '[data-cogwheel-menu]', voteit.load_cog_menu);
+    $('body').on('click', '[data-set-poll]', voteit.set_poll_proposal);
+    $('body').on('click', '[data-close-pick-poll]', voteit.close_pick_poll);
+    $('body').on('click', '[data-pick-poll]', voteit.pick_poll);
 });
