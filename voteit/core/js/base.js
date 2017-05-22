3de65bfac3e737f5db180d7a4dc4ff79d5f2cc84
Robin Harms Oredsson <robin@betahaus.net>
2017-05-22 21:22:53 +0200

    Note to fix later

--- voteit/core/js/base.js
+++ voteit/core/js/base.js
@@ -1,13 +1,26 @@
 
-//Scroll extension
 (function($) {
+    //Scroll extension
     $.fn.goTo = function() {
         $('html, body').animate({
             scrollTop: $(this).offset().top - 100 + 'px'
         }, 'fast');
         return this; // for chaining...
     }
+
+/* FIXME
+    $.fn.linkHashtag = function(insert) {
+        var insert = (typeof insert == 'undefined') ? '$1<a data-tag-filter="$2" href="$2">$2</a>' : insert;
+        //var pattern = '/(^|\W)(#[a-z\d][\w-]*)/ig';
+        //var pattern2 = '(?<=\s|^)#(\w*[A-Za-z_]+\w*)'
+        //$(this).html( $(this).html().replace(/(^|\W)(#[a-z\d][\w-]*)/ig, insert) );
+        //$(this).html( $(this).html().replace(/(\W+)(#[a-z\d][\w-]*)/ig, insert) );
+        $(this).html( $(this).html().replace(/(^|\s)(#[a-z\d-]+)/ig, insert) );
+    }
+    */
+
 })(jQuery);
+
 
 /* Main configuration */
 if(typeof(voteit) == "undefined"){
