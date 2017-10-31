c4d82c46eb1cfa49c0eb58a1128a600560622f72
Robin Harms Oredsson <robin@betahaus.net>
2017-10-31 11:36:44 +0100

    Don't show inside meetings

--- voteit/core/views/__init__.py
+++ voteit/core/views/__init__.py
@@ -1,8 +1,7 @@
 
-
 def render_actionbar(view, **kw):
     """ Called from Arche to request an actionbar. We only show it for moderators and admins. """
-    if view.request.is_moderator:
+    if not view.request.meeting and view.request.is_moderator:
         return view.render_template('arche:templates/action_bar.pt')
 
 def includeme(config):
