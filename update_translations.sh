a5b7af482e58f8aaa2a7ec16ef490bae253f4388
Robin Harms Oredsson <robin@betahaus.net>
2018-02-21 14:57:02 +0100

    Better translation script

--- update_translations.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- #You need lingua and gettext installed to run this
- 
- echo "Updating voteit.core.pot"
- pot-create -d voteit.core -o voteit/core/locale/voteit.core.pot .
- echo "Merging Swedish localisation"
- msgmerge --update voteit/core/locale/sv/LC_MESSAGES/voteit.core.po voteit/core/locale/voteit.core.pot
- echo "Updated locale files"
- 
