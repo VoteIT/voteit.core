c81ed837c8a995762daaa7b58701ab1cb71f4e50
Robin Harms Oredsson <robin@betahaus.net>
2018-06-18 11:42:07 +0200

    Saner structure for scss files so they can be replaced easier

--- voteit/core/fanstaticlib.py
+++ voteit/core/fanstaticlib.py
@@ -18,6 +18,8 @@ deformlib = Library('deform', resource_filename('deform', 'static'))
 voteit_core_jslib = Library('voteit_js', 'js')
 voteit_core_csslib = Library('voteit_css', 'css')
 
+voteit_main_css = Resource(voteit_core_csslib, 'main.css', depends=(bootstrap_css, main_css), dont_bundle = True)
+
 base_js = Resource(voteit_core_jslib, 'base.js', depends = (common_js, jquery))
 data_loader = Resource(voteit_core_jslib, 'data_loader.js', depends = (base_js, pure_js,))
 unread_js = Resource(voteit_core_jslib, 'unread.js', depends = (base_js, data_loader))
@@ -25,13 +27,7 @@ watcher_js = Resource(voteit_core_jslib, 'watcher.js', depends = (data_loader,))
 like_js = Resource(voteit_core_jslib, 'like.js', depends = (data_loader,))
 support_js = Resource(voteit_core_jslib, 'support.js', depends = (data_loader,))
 navs_js = Resource(voteit_core_jslib, 'navs.js', depends = (data_loader, watcher_js))
-
-#voteit_bootstrap = Resource(voteit_core_csslib, 'voteit_bootstrap.css')
-#Warning, bundling causes this resource to fail. It may have something to do with the css-map. (Probably not an accessible directory)
-voteit_main_css = Resource(voteit_core_csslib, 'main.css', depends=(bootstrap_css, main_css), dont_bundle = True)
-
 participants_js = Resource(voteit_core_jslib, 'participants.js', depends = (base_js, pure_js))
-
 voteit_moderator_js = Resource(voteit_core_jslib, 'voteit_moderator.js', bottom = True, depends = (data_loader,))
 voteit_manage_tickets_js = Resource(voteit_core_jslib, 'voteit_manage_tickets.js', bottom = True, depends=(tablesorter,))
 
