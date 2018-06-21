ace6dc4a2ca31f32eca2f9fbb7d07db795a91b7a
Robin Harms Oredsson <robin@betahaus.net>
2018-06-21 14:31:21 +0200

    Cleanup and set images when reasonable

--- voteit/core/plugins/arche_pas_image.py
+++ voteit/core/plugins/arche_pas_image.py
@@ -1,3 +1,6 @@
+
+from arche.interfaces import IObjectUpdatedEvent
+from arche.interfaces import IWillLoginEvent
 try:
     from arche_pas.interfaces import IProviderData
     from arche_pas.interfaces import IPASProvider
@@ -34,6 +37,28 @@ class PASImagePlugin(object):
         return bool(provider.get_profile_image(self.provider_data.get(self.pas_name, {})))
 
 
+def use_provider_image_if_none_present(event):
+    provider_name = getattr(event, 'provider', None)
+    if not provider_name:
+        return
+    if event.user.profile_image_plugin:
+        return
+    request = getattr(event, 'request', get_current_request())
+    provider = request.registry.queryAdapter(request, IPASProvider, name=provider_name)
+    data = IProviderData(event.user, {})
+    img_url = provider.get_profile_image(data.get(provider_name, {}))
+    if img_url:
+        event.user.profile_image_plugin = "pas_image_%s" % provider_name
+
+
+def remove_image_if_pas_removed(user, event):
+    if 'pas_ident' not in getattr(event, 'changed', ()):
+        return
+    data = IProviderData(user, {})
+    if user.profile_image_plugin and user.profile_image_plugin not in data:
+        user.profile_image_plugin = ""
+
+
 def includeme(config):
     """ Include plugins matching IPASProvider adapters
     """
@@ -45,7 +70,8 @@ def includeme(config):
                     name = "pas_image_%s" % ar.name
                     pas_name = ar.name
                     title = ar.factory.title
-                #IPASProvider
                 config.registry.registerAdapter(_Adapter, name = _Adapter.name)
     else:
         raise ImportError("arche_pas not included")
+    config.add_subscriber(use_provider_image_if_none_present, IWillLoginEvent)
+    config.add_subscriber(remove_image_if_pas_removed, [IUser, IObjectUpdatedEvent])
