13b647bb84b83e59940c39a3f106387ec4e18a87
Robin Harms Oredsson <robin@betahaus.net>
2020-05-07 13:33:38 +0200

    Applying black

--- voteit/core/plugins/gravatar_profile_image.py
+++ voteit/core/plugins/gravatar_profile_image.py
@@ -11,23 +11,25 @@ from voteit.core.models.interfaces import IProfileImage
 @implementer(IProfileImage)
 @adapter(IUser)
 class GravatarProfileImagePlugin(object):
-    name = u'gravatar_profile_image'
-    title = _('Gravatar')
-    description = _(u'profile_gravatar_explanation',
-                    default=u'Profile image from <a href="http://www.gravatar.com" target="_blank">Gravatar network</a>.' \
-                    'It\'s taken from your current email address. If you want to change the picture, simply go to' \
-                    'the Gravatar site and change your picture for the email you use in VoteIT.')
+    name = u"gravatar_profile_image"
+    title = _("Gravatar")
+    description = _(
+        "profile_gravatar_explanation",
+        default='Profile image from <a href="http://www.gravatar.com" target="_blank">Gravatar network</a>. '
+        "It's taken from your current email address. If you want to change the picture, simply go to "
+        "the Gravatar site and change your picture for the email you use in VoteIT.",
+    )
 
     def __init__(self, context):
         self.context = context
 
     def url(self, size, request):
-        url = 'https://secure.gravatar.com/avatar/'
-        email = self.context.get_field_value('email', '').strip().lower()
+        url = "https://secure.gravatar.com/avatar/"
+        email = self.context.get_field_value("email", "").strip().lower()
         if email:
             url += md5(email).hexdigest()
-        url += '?s=%s' % size
-        url += '&d=%s' % request.registry.settings.get('voteit.gravatar_default', 'mm')
+        url += "?s=%s" % size
+        url += "&d=%s" % request.registry.settings.get("voteit.gravatar_default", "mm")
         return url
 
     def is_valid_for_user(self):
@@ -37,4 +39,6 @@ class GravatarProfileImagePlugin(object):
 def includeme(config):
     """ Include gravatar plugin
     """
-    config.registry.registerAdapter(GravatarProfileImagePlugin, name = GravatarProfileImagePlugin.name)
+    config.registry.registerAdapter(
+        GravatarProfileImagePlugin, name=GravatarProfileImagePlugin.name
+    )
