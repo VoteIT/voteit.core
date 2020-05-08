e08913619ba2a94eec9ac6a8c3880d10afb348f5
Robin Harms Oredsson <robin@betahaus.net>
2020-05-08 09:29:01 +0200

    The extra check for moderator perms was missing on the ajax response view

--- voteit/core/views/base_inline.py
+++ voteit/core/views/base_inline.py
@@ -7,12 +7,17 @@ class ProposalInlineMixin(object):
     """
 
     def get_context_response(self):
-        response = {}
-        response['docids'] = [0]  # Not used
-        response['unread_docids'] = ()
-        response['contents'] = iter([self.context])
-        response['hidden_count'] = False
-        response['context'] = self.context.__parent__  # AI
+        response = {
+            "docids": [0],
+            "unread_docids": (()),
+            "contents": iter([self.context]),
+            "hidden_count": False,
+            "context": self.context.__parent__,
+            "ck_mod": lambda obj: self.request.has_permission(
+                security.MODERATE_MEETING, self.context
+            ),
+        }
+        # A more expensive but accurate permission check for a specific object.
         return response
 
 
@@ -20,10 +25,12 @@ class PollInlineMixin(object):
     """ Ment to be used as mixin when one object is changed and
         should be returned inline.
     """
+
     def get_context_response(self):
-        response = {}
-        response['contents'] = iter([self.context])
-        response['vote_perm'] = security.ADD_VOTE
-        response['context'] = self.context.__parent__  # AI
-        response['show_add'] = False
+        response = {
+            "contents": iter([self.context]),
+            "vote_perm": security.ADD_VOTE,
+            "context": self.context.__parent__,
+            "show_add": False,
+        }
         return response
